41e1375c2c5d12bc3163e9ff7c3a76503e4a88d7
Christian <christianfryksten1@gmail.com>
2025-08-19 11:38:19 +0200

    Test commit

---
+++ src/main/java/org/example/ElectricPricesAPI.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class ElectricPricesAPI {
+
+    public static String getTodaysElectricalPrices() {
+
+
+        return "In progress";
+    }
+
+    public static String getTomorrowsElectricalPrices() {
+
+        return "In progress";
+
+    }
+
+}
+

bd0682d687674a69e0b64b726d2b914bb42f447a
Henrik <Henrikmattsson89@gmail.com>
2025-08-19 10:52:06 +0200

    added and tested ZonePicker CLI

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,17 @@
 package org.example;
 
 import org.example.api.ElprisApi;
+import org.example.cli.ZonePicker;
 
 import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) throws Exception {
         ElprisApi api = new ElprisApi();
-        String response = api.getRequest(LocalDate.now(), "SE3");
+        String zone = new ZonePicker().getZone();
+        String response = api.getRequest(LocalDate.now(), zone);
         System.out.println(response);
+
+
     }
 }

dbd6550544b7da93c4925e680240e208579af7f4
Alfred Brännare <alfred-brannare@hotmail.com>
2025-08-19 18:41:22 +0200

    feat: Set up a menu structure.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,11 +5,32 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        String URL = "https://www.elprisetjustnu.se/api/v1/prices/2025/08-19_SE3.json";
-        getPrices(URL);
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("Choose option:");
+            System.out.println("1. Download prices for the current day and next day");
+            System.out.println("2. Print the mean price for the current 24-hour period");
+            System.out.println("3. Identify and print the hours with the cheapest and most expensive prices");
+            System.out.println("4. Exit");
+
+            if (scanner.hasNextInt()) {
+                choice = scanner.nextInt();
+            } else {
+                System.out.println("Invalid input, choose again");
+                scanner.next();
+                choice = -1;
+            }
+        } while (choice < 1 || choice > 4);
+
+
+        /* String URL = "https://www.elprisetjustnu.se/api/v1/prices/2025/08-19_SE3.json";
+        getPrices(URL); */
     }
 
     static void getPrices(String url_string) {
@@ -21,6 +42,7 @@ public class App {
             String response;
             while ((response = bufferedReader.readLine()) != null) {
                 System.out.println(response);
+                bufferedReader.close();
             }
         } catch (Exception e) {
             e.printStackTrace();

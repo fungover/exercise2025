c76dd3d32dcab944d9f4bc6c9a0df1292cfdb728
Henrik <Henrikmattsson89@gmail.com>
2025-08-19 10:15:55 +0200

    setup the ElprisAPI and checked if it worked

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.example.api.ElprisApi;
+
+import java.time.LocalDate;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello There!");
+    public static void main(String[] args) throws Exception {
+        ElprisApi api = new ElprisApi();
+        String response = api.getRequest(LocalDate.now(), "SE3");
+        System.out.println(response);
     }
 }

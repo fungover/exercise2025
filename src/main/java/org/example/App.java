65ea959b849d04efbf2efdd6442b428c42a70627
Martin Blomberg <martin.blomberg@outlook.com>
2025-08-20 10:02:49 +0200

    Loops demo

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,33 @@
 package org.example;
 
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        int i = 0;
+        while (i < 3) {
+            System.out.println("Hello World!");
+            i++;
+        }
+        for(int j = 0; j < 3; j++) {
+            System.out.println("Hello World!");
+        }
+        //Loop using recursion, be carful to not fill the stack == stack overflow
+        counter(0);
     }
+
+    public static void counter(int i) {
+        if( i == 3)
+            return;
+        System.out.println(i);
+        counter(i + 1);
+    }
+
+    public static void demo() {
+        System.out.println("Demo");
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
 }

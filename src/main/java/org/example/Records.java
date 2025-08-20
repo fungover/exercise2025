b0597abd7925174363ff5044dbc45d366e6d61af
Martin Blomberg <martin.blomberg@outlook.com>
2025-08-20 11:39:42 +0200

    Komplexa objekt med klass och record

---
+++ src/main/java/org/example/Records.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Records {
+
+    static void main() {
+        final Box box = new Box();
+
+        box.width = 10;
+        box.height = 20;
+        box.length = 15;
+
+        BoxRecord boxRecord = new BoxRecord(10, 20, 15);
+        BoxRecord largeBox = new BoxRecord(100, 200, 100);
+        BoxRecord smallBox = new BoxRecord(10, 20, 15);
+        var anotherLargeBox = new BoxRecord(100, 200, 100);
+
+        if( largeBox.equals( anotherLargeBox ))
+            System.out.println("Large Box is equal to AnotherLarge Box");
+    }
+
+
+}
+
+class Box {
+    int width;
+    int height;
+    int length;
+}
+
+//Immutable - kan inte ändras efter skapandet
+record BoxRecord(int width, int height, int length) {}

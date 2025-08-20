428a21dfaa8113673ec2f185cb7d8370fc9af5f6
Alfred Brännare <alfred-brannare@hotmail.com>
2025-08-20 13:41:05 +0200

    Extracted the input handling logic into a separate getChoice method

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,29 +9,40 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("Choose option:");
+
         Scanner scanner = new Scanner(System.in);
+        getChoice(scanner);
+
+        /* String URL = "https://www.elprisetjustnu.se/api/v1/prices/2025/08-19_SE3.json";
+        getPrices(URL); */
+    }
+
+    private static int getChoice(Scanner scanner) {
         int choice;
 
         do {
-            System.out.println("Choose option:");
             System.out.println("1. Download prices for the current day and next day");
             System.out.println("2. Print the mean price for the current 24-hour period");
             System.out.println("3. Identify and print the hours with the cheapest and most expensive prices");
             System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
 
             if (scanner.hasNextInt()) {
                 choice = scanner.nextInt();
+                if (choice < 1 || choice > 4) {
+                    System.out.println("Invalid choice, must be between 1 and 4");
+                }
             } else {
-                System.out.println("Invalid input, choose again");
+                System.out.println("Invalid choice, must be a number between 1 and 4");
                 scanner.next();
                 choice = -1;
             }
         } while (choice < 1 || choice > 4);
 
-
-        /* String URL = "https://www.elprisetjustnu.se/api/v1/prices/2025/08-19_SE3.json";
-        getPrices(URL); */
+        return choice;
     }
+
 
     static void getPrices(String url_string) {
         try {

5be18bbe805eabf5e0a2d05aa32a54fe7b68b577
Rikard <rikardengstrom89@gmail.com>
2025-08-21 19:14:41 +0200

    Added another menu option. So that the user get a submenu after first selection.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,21 @@
 package org.example;
 
 import org.example.api.ApiClient;
+import org.example.cli.Menu;
 import org.example.cli.ZoneSelection;
-import org.example.utils.CalculateMeanPrice;
-
-import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
+        ZoneSelection zoneSelection = new ZoneSelection();
+        String zone = zoneSelection.chooseZone();
+
+        ApiClient apiClient = new ApiClient();
+        String jsonToday = apiClient.getRequest(java.time.LocalDate.now(), zone);
+        String jsonTomorrow = apiClient.getRequest(java.time.LocalDate.now().plusDays(1), zone);
+
+        Menu menu = new Menu();
+        menu.showMenu(jsonToday, jsonTomorrow);
 
     }
 }

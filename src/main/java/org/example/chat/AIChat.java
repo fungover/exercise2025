035458cc938cd0d704b4ced6952ebb91899d56ac
Martin Blomberg <martin.blomberg@outlook.com>
2025-08-21 12:53:57 +0200

    Prepare exercise for calling llm on openrouter

---
+++ src/main/java/org/example/chat/AIChat.java
@@ -0,0 +1,13 @@
+package org.example.chat;
+
+public class AIChat {
+
+    //Todo: Send user query to OpenRouter AI using java HttpClient.
+    //Todo: Add support for setting API-Key using dotenv-java
+    //Todo: Convert json response to record Objects using Jackson and print the answer on screen.
+    //Todo: Create a complete chat conversation by including previous messages in next query for context
+
+    static void main() {
+
+    }
+}

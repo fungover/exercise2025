6616d999acab1d0beb1889b8b99c93b8b4724a3e
Alfred Brännare <alfred-brannare@hotmail.com>
2025-08-23 11:03:00 +0200

    Add functionality to download today's and tomorrow's electricity prices

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,15 +1,23 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Choose option:");
+        System.out.println("=== Electricity Price Tool Menu ===");
 
         Scanner scanner = new Scanner(System.in);
         String area = getArea(scanner);
@@ -23,6 +31,7 @@ public class App {
 
     private static String getArea(Scanner scanner) {
         int area;
+        System.out.println("Choose area:");
 
         do {
             System.out.println("1. Luleå / Northern Sweden");
@@ -76,9 +85,70 @@ public class App {
 
         switch (choice) {
             case 1:
+                LocalDate today = LocalDate.now();
+                LocalDate tomorrow = today.plusDays(1);
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd");
+
+                String date = today.format(formatter);
+                String dateTomorrow = tomorrow.format(formatter);
+
                 System.out.println("Downloading prices for the current day and next day");
-                String result = urlBuilder( "123", area);
-                System.out.println(result);
+                String urlToday = urlBuilder( date, area);
+                String urlTomorrow = urlBuilder( dateTomorrow, area);
+
+                //Today's prices
+                HttpResponse<String> todayPrices = getPrices(urlToday);
+                if (todayPrices != null && todayPrices.statusCode() == 200) {
+                    System.out.println("Today's prices downloaded successfully!");
+
+                    ObjectMapper mapper = new ObjectMapper();
+                    mapper.findAndRegisterModules();
+
+                    try {
+                        PriceEntry[] entries = mapper.readValue(todayPrices.body(), PriceEntry[].class);
+                        System.out.println("Today's prices:");
+                        createFile("prices.txt");
+
+                        boolean firstLine = true;
+
+                        for (PriceEntry entry : entries) {
+                            if (firstLine) {
+                                System.out.println("=====" + today + "=====");
+                                writeToFile("prices.txt", "=====" + today + "=====\n");
+                                firstLine = false;
+                            }
+                            System.out.println(entry.time_start + " - " + entry.time_end + ": " + entry.SEK_per_kWh);
+                            writeToFile("prices.txt", entry.time_start + " - " + entry.time_end + ": " + entry.SEK_per_kWh + " SEK_per_kWh" + "\n");
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                } else {
+                    System.out.println("Today's prices could not be downloaded. Status: " +
+                            (todayPrices != null ? todayPrices.statusCode() : "No response"));
+                }
+
+                //Tomorrow's prices'
+                HttpResponse<String> tomorrowPrices = getPrices(urlTomorrow);
+                if (tomorrowPrices != null && tomorrowPrices.statusCode() == 200) {
+                    System.out.println("Tomorrow's prices downloaded successfully!");
+
+                    ObjectMapper mapper = new ObjectMapper();
+                    mapper.findAndRegisterModules();
+
+                    try {
+                        PriceEntry[] entries = mapper.readValue(tomorrowPrices.body(), PriceEntry[].class);
+                        System.out.println("Tomorrow's prices: " + tomorrowPrices.body());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                } else {
+                    System.out.println("Tomorrow's prices could not be downloaded. Status: " +
+                            (tomorrowPrices != null ? tomorrowPrices.statusCode() : "No response"));
+                }
+
                 break;
             case 2:
                 System.out.println("Printing the mean price for the current 24-hour period");
@@ -102,20 +172,49 @@ public class App {
         return "https://www.elprisetjustnu.se/api/v1/prices/" + year + "/" + date + "_" + zone + ".json";
     }
 
-    static void getPrices(String url_string) {
+    private static HttpResponse<String> getPrices(String url_string) {
+        HttpResponse<String> response = null;
         try {
-            URL url = new URL(url_string);
-            URLConnection connection = url.openConnection();
-            InputStream inputStream = connection.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            String response;
-            while ((response = bufferedReader.readLine()) != null) {
-                System.out.println(response);
-                bufferedReader.close();
-            }
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url_string))
+                    .build();
+
+        response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        return response;
     }
+
+    private static void createFile(String fileName) {
+        try {
+           File myObj = new File("prices.txt");
+           if (myObj.createNewFile()) {
+               System.out.println("File created: " + myObj.getName());
+           } else {
+               System.out.println("File already exists.");
+           }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeToFile(String fileName, String content) {
+        try {
+            FileWriter myWriter = new FileWriter(fileName, true);
+            myWriter.write(content);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {}
+    }
+
+    public record PriceEntry(
+            double SEK_per_kWh,
+            double EUR_per_kWh,
+            double EXR,
+            OffsetDateTime time_start,
+            OffsetDateTime time_end) {}
 }

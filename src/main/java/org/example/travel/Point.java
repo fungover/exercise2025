ef5b3b7aab898a292659a225296a134ceda41eaf
Martin Blomberg <martin.blomberg@outlook.com>
2025-08-26 09:15:45 +0200

    Add fields and construtor to Point

--- src/main/java/org/example/travel/Point.java
+++ src/main/java/org/example/travel/Point.java
@@ -2,7 +2,22 @@ package org.example.travel;
 
 public class Point {
 
-    //This class should represent a point in a 2D plane.
+    private final double x;
+    private final double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
 
     //We should be able to calculate the distance between two points.
     //We should be able to display the point.

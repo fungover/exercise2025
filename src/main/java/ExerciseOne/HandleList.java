1632135a51e207dca70cbd7e55d800d2aea7088c
Louise <emmalouisekarlsson@hotmail.com>
2025-08-26 10:18:43 +0200

    Stores data from request as a list.

---
+++ src/main/java/ExerciseOne/HandleList.java
@@ -0,0 +1,21 @@
+package ExerciseOne;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HandleList {
+
+  private final List<Pricing> listOfPrices =  new ArrayList<>();
+
+    public void addList(List<Pricing> priceList) {
+       listOfPrices.addAll(priceList);
+    }
+
+    public List<Pricing> getListOfPrices() {
+        return listOfPrices;
+    }
+
+    public void printList(){
+        System.out.println("LIST"+listOfPrices);
+    }
+}

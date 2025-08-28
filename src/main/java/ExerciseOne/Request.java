7aeafc13419c3b41edf1e3507f608cd80d0b4274
Louise <emmalouisekarlsson@hotmail.com>
2025-08-28 13:19:42 +0200

    Added if statement in case tomorrow price list not yet are available

--- src/main/java/ExerciseOne/Request.java
+++ src/main/java/ExerciseOne/Request.java
@@ -7,6 +7,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.List;
 
 //https://www.elprisetjustnu.se/api/v1/prices/[ÅR]/[MÅNAD]-[DAG]_[PRISKLASS].json
@@ -25,10 +26,15 @@ public class Request {
 
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            ObjectMapper mapper = new ObjectMapper();
-            List<Pricing> pricing = mapper.readValue(response.body(), new TypeReference<List<Pricing>>() {});
+            List<Pricing> responseList = new ArrayList<>();
 
-            return pricing;
+            if(response.statusCode() == 200 && response.body().startsWith("[")){
+                ObjectMapper mapper = new ObjectMapper();
+                List<Pricing> pricing = mapper.readValue(response.body(), new TypeReference<List<Pricing>>() {});
+                responseList.addAll(pricing);
+            }
+
+            return responseList;
 
     }
 }

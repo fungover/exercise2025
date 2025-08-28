fe7f20fe6a0b7da917a2b7a403cb9b167dfe922e
0x0xoscar <139160108+0x0xoscar@users.noreply.github.com.>
2025-08-28 15:00:16 +0200

    Added PriceEntry and mapper

--- src/main/java/org/example/ElectricityPriceFetcher.java
+++ src/main/java/org/example/ElectricityPriceFetcher.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -7,12 +10,16 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ElectricityPriceFetcher {
     private final ZoneId zoneId = ZoneId.of("Europe/Stockholm");
     private LocalDate today = LocalDate.now(zoneId);
     private LocalDate tomorrow = today.plusDays(1);
     private final HttpClient httpClient = HttpClient.newHttpClient();
+    private final ObjectMapper mapper = new ObjectMapper();
 
     private String buildUrl(LocalDate date, String zone) {
         // Use %02d for minimum 2 in width adds 0 before if needed.
@@ -20,21 +27,25 @@ public class ElectricityPriceFetcher {
                 date.getYear(), date.getMonthValue(), date.getDayOfMonth(), zone);
     }
 
-    public void fetchPrices(LocalDate date, String zone) throws IOException, InterruptedException {
+    public List<PriceEntry> fetchPrices(LocalDate date, String zone) throws IOException, InterruptedException {
         String url = buildUrl(date, zone);
 
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-        if (response.statusCode() == 200) {
-            System.out.println(response.body());
+        if (response.statusCode() == 404) {
+            return List.of();
         }
 
+        return mapper.readValue(response.body(), new TypeReference<List<PriceEntry>>() {});
     }
 
-    public void getTodayAndTomorrow(String zone) throws IOException, InterruptedException {
-        fetchPrices(today, zone);
-        fetchPrices(tomorrow, zone);
+    public List<PriceEntry> getTodayAndTomorrow(String zone) throws IOException, InterruptedException {
+        List<PriceEntry> combined = new ArrayList<>();
+        combined.addAll(fetchPrices(today, zone));
+        combined.addAll(fetchPrices(tomorrow, zone));
+
+        return combined;
     }
 
 }

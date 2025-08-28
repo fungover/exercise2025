2a2871dae5033ce54e60b7edea4a09e2cd7b5c07
Rikard <rikardengstrom89@gmail.com>
2025-08-28 08:17:34 +0200

    Just pushing branch for exercise2.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        System.out.println("Hello There! just a quick test to see it works!");
     }
 }

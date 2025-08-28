f0f65c1181973a852d5c10ef51dfc1047cf6a805
Patrik <pat_p3@hotmail.com>
2025-08-28 15:47:56 +0200

    Add overflow guard for startIndex + length

--- src/main/java/org/example/model/BestChargingTimes.java
+++ src/main/java/org/example/model/BestChargingTimes.java
@@ -6,6 +6,8 @@ public class BestChargingTimes {
             if (startIndex < 0) throw new IllegalArgumentException("startIndex must be >= 0");
             if (length <= 0) throw new IllegalArgumentException("length must be > 0");
             if (totalSek < 0) throw new IllegalArgumentException("totalSek must be >= 0");
+            if (startIndex > Integer.MAX_VALUE - length) throw new
+                    IllegalArgumentException("startIndex + lenght overflows int range");
         }
     }
 

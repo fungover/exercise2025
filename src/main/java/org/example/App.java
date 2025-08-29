086fbc963fd04bc12dace81f97d04d888e5ce4b8
ViktorNoskire <vikkerinho@gmail.com>
2025-08-30 00:02:32 +0200

    Format code for better SOC

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,34 @@
+// App.java
 package org.example;
-import org.example.elecPrice.ElectricityPrices;
+
+import org.example.model.Hour;
+import org.example.service.ElectricityPriceService;
+
 import java.io.IOException;
+import java.util.List;
+
 public class App {
-	static void main(String[] args) throws IOException, InterruptedException {
-		ElectricityPrices electricityPrices = new ElectricityPrices();
-		electricityPrices.run();
+	public static void main(String[] args) throws IOException, InterruptedException {
+		System.out.print("Electricity zone: ");
+		String zone = System.console().readLine().trim().toUpperCase();
+
+		ElectricityPriceService service = new ElectricityPriceService(zone);
+		List<Hour> prices = service.getElectricityPrices();
+
+		double lowest = 0.0;
+		double total = 0.0;
+		int lowestHour = 0;
+		for (Hour h : prices) {
+			lowest = h.formatHour(h.time_start());
+			total += h.SEK_per_kWh();
+			if (h.SEK_per_kWh() < lowest) {
+				lowest = h.SEK_per_kWh();
+				lowestHour = h.formatHour(h.time_start());
+			}
+		}
+
+		double avg = total /  prices.size();
+		System.out.printf("Lowest price: %.2f kr/kWh at %02d:00%n", lowest, lowestHour);
+		System.out.printf("Mean price: %.2f kr/kWh%n", avg);
 	}
 }
--- src/main/java/org/example/model/Hour.java
+++ src/main/java/org/example/model/Hour.java
@@ -1,4 +1,4 @@
-package org.example.elecPrice;
+package org.example.model;
 
 import java.time.OffsetDateTime;
 

cee1ab58eb55685450e5928a6948ed358577cae7
Deansie <dennis-andersen@outlook.com>
2025-08-29 11:00:21 +0200

    refactor(PriceUtil): update PriceUtil to support HourlyPrice and dualcurrency convertions

--- src/main/java/org/example/util/PriceUtil.java
+++ src/main/java/org/example/util/PriceUtil.java
@@ -1,28 +1,37 @@
 package org.example.util;
-import com.fasterxml.jackson.jr.ob.JSON;
 import org.example.model.MeanPrice;
-import java.net.URL;
+import org.example.model.HourlyPrice;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class PriceUtil {
-
-    public static MeanPrice calculateMeanPrice(URL url) throws Exception {
-        List<Object> results = JSON.std.listFrom(url);
-
-        double sumSek = 0.0;
-        double sumEur = 0.0;
-
-        for (Object o : results) {
-            Map<String, Object> map = (Map<String, Object>) o;
-            sumSek += ((Number) map.get("SEK_per_kWh")).doubleValue();
-            sumEur += ((Number) map.get("EUR_per_kWh")).doubleValue();
-        }
-
-        double meanSek = sumSek / results.size();
-        double meanEur = sumEur / results.size();
-
-        return new MeanPrice(meanSek, meanEur);
+    public static MeanPrice calculateMeanPrice(List<HourlyPrice> prices) {
+        if (prices.isEmpty()) return new MeanPrice(0.0, 0.0);
+        double sumSek = prices.stream().mapToDouble(HourlyPrice::getSekPerKwh).sum();
+        double sumEur = prices.stream().mapToDouble(HourlyPrice::getEurPerKwh).sum();
+        return new MeanPrice(sumSek / prices.size(), sumEur / prices.size());
     }
 
+public static List<HourlyPrice> convertToHourlyPrices(List<Object> priceData, String zone) {
+
+    List<HourlyPrice> prices = new ArrayList<>();
+
+    for (Object o : priceData)  {
+    Map<String, Object> map = (Map<String, Object>) o;
+    int hour = parseHour((String) map.get("time_start"));
+        double sekPerKwh = ((Number) map.get("SEK_per_kWh")).doubleValue();
+        double eurPerKwh = ((Number) map.get("EUR_per_kWh")).doubleValue();
+        prices.add(new HourlyPrice(hour, sekPerKwh, eurPerKwh, zone));
+    }
+            return prices;
+    }
+
+    public static String formatPrice(double price) {
+        return String.format("%.2f", price);
+    }
+
+    private static int parseHour(String timeStart) {
+        return Integer.parseInt(timeStart.split("T")[1].split(":")[0]);
+    }
 }

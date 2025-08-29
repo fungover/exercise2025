d1450d068007f492870a532a51e72740f3319da8
Louise <emmalouisekarlsson@hotmail.com>
2025-08-29 09:06:57 +0200

    Added zone id

--- src/main/java/ExerciseOne/GetDateAndTime.java
+++ src/main/java/ExerciseOne/GetDateAndTime.java
@@ -3,23 +3,26 @@ package ExerciseOne;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class GetDateAndTime {
 
+    private final ZoneId zoneId = ZoneId.of("Europe/Stockholm");
+
     public String getToday(int area) {
 
-        String today = formatDate(LocalDate.now());
-        return today+"_SE"+area;
+        String today = formatDate(LocalDate.now(this.zoneId));
+        return today + "_SE" + area;
     }
 
     public String getTomorrow(int area) {
-        String tomorrow = formatDate(LocalDate.now().plusDays(1));
-        return tomorrow+"_SE"+area;
+        String tomorrow = formatDate(LocalDate.now(this.zoneId).plusDays(1));
+        return tomorrow + "_SE" + area;
     }
 
     public int getHour(){
-        LocalTime time = LocalTime.now();
+        LocalTime time = LocalTime.now(this.zoneId);
         DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("HH");
         String  formatedTime = time.format(dtf2);
 

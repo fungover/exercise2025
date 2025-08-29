78fa6ea60a2ce8bf6bdc01105c89e4b41a142b18
RobTrb <Robert.trb@gmail.com>
2025-08-29 22:02:57 +0200

    First commit for exercise 1. Just added the messages, input prompt and a check to make sure the input is accepted.

---
+++ src/main/java/org/example/electriCheck.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class electriCheck {
+
+    static Scanner scanner = new Scanner(System.in);
+    public static String zone = "";
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to ElectriCheck \n This is a simple electricity price checking software. \n This program will download the electricity prices for the upcoming 24 hours \n (note that upcoming prices are released after 1 PM so checking before that will not take the full 24 hours into account) \n");
+        System.out.println("Please input your price zone? \n \n SE1 = Luleå / Norra Sverige \n SE2 = Sundsvall / Norra Mellansverige \n SE3 = Stockholm / Södra Mellansverige \n SE4 = Malmö / Södra Sverige \n");
+
+        do {
+            System.out.print("Zone: ");
+            zone = scanner.nextLine();
+
+            if (!zone.equalsIgnoreCase("SE1") && !zone.equalsIgnoreCase("SE2") && !zone.equalsIgnoreCase("SE3") && !zone.equalsIgnoreCase("SE4")) {
+                System.out.println("Invalid input. Please try again");
+            }
+        } while (!zone.equalsIgnoreCase("SE1") && !zone.equalsIgnoreCase("SE2") && !zone.equalsIgnoreCase("SE3") && !zone.equalsIgnoreCase("SE4"));
+
+        System.out.println(zone);
+    }
+}

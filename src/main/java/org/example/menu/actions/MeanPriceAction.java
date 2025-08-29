eb2b6e64ae3641f3d6d2c5641749915a8b5fe12c
Deansie <dennis-andersen@outlook.com>
2025-08-29 12:37:48 +0200

    feat(menu): implement MeanPriceAction for displaying mean prices

--- src/main/java/org/example/menu/actions/MeanPriceAction.java
+++ src/main/java/org/example/menu/actions/MeanPriceAction.java
@@ -1,4 +1,26 @@
 package org.example.menu.actions;
 
-public class MeanPriceAction {
+import org.example.data.PriceDataManager;
+import org.example.menu.MenuAction;
+import org.example.model.MeanPrice;
+import org.example.util.PriceUtil;
+
+public class MeanPriceAction implements MenuAction {
+    private final PriceDataManager dataManager;
+
+    public MeanPriceAction(PriceDataManager dataManager) {
+        this.dataManager = dataManager;
+    }
+
+    @Override
+    public void execute() {
+        MeanPrice meanPrice = PriceUtil.calculateMeanPrice(dataManager.getPrices());
+        System.out.println("Mean price for zone " + dataManager.getZone() + ":");
+        System.out.println(meanPrice);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Calculate the mean price for the current day";
+    }
 }

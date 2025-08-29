10b97856ca818bf9b6879234b307623149cd917a
Magnus <49763392+MagnusTerak@users.noreply.github.com>
2025-08-29 18:07:04 +0200

    Branch init & Zone selection

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,26 @@
 package org.example;
 
+//System.console().readLine()
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        String zone = zoneSelection();
+        System.out.println("Selected zone: " + zone);
+    }
+
+    public static String zoneSelection() {
+        System.out.println("Welcome! Please select what zone you would like to see the electrical prices in.");
+        System.out.println("(1) - Luleå / Norra Sverige");
+        System.out.println("(2) - Sundsvall / Norra Mellansverige");
+        System.out.println("(3) - Stockholm / Södra Mellansverige");
+        System.out.println("(4) - Malmö / Södra Sverige");
+        System.out.println("Please select what zone you would like to see with the corresponding number");
+
+        Scanner scanner = new Scanner(System.in);
+        String selection = scanner.nextLine();
+
+        return "SE" + selection;
     }
 }

d87846451846753ca25f3f971666514d1c60f9f4
Kirill <kirillsavorin@gmail.com>
2025-08-30 09:09:29 +0200

    Init & Simple menu and main functions

---
+++ src/main/java/cheap/electricity/cli/Main.java
@@ -0,0 +1,13 @@
+package cheap.electricity.cli;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Let's check when it's better to start charging");
+    System.out.println("Choose from the menu:");
+    System.out.println("1. Download prices(day1, day2)");
+    System.out.println("2. Show mean price(24h)");
+    System.out.println("3. Show price highest and lowest(h)");
+    System.out.println("4. The best time to charge a car");
+    System.out.println("6. Choose zon(default is: ZONE2)");
+  }
+}

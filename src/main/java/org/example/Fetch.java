62ddfa2c5a81402e9ad76d2bb1f47838746a3462
Patseroni <patrik.bustad@lerniastudent.se>
2025-09-01 01:23:38 +0200

    calculate mean, most expensive and cheapest price for the current day

--- src/main/java/org/example/Fetch.java
+++ src/main/java/org/example/Fetch.java
@@ -1,27 +1,43 @@
 package org.example;
 import java.io.IOException;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
+
 class Fetch {
-    public void fetch() throws IOException, InterruptedException {
-        String url = "https://www.elprisetjustnu.se/api/v1/prices/2025/08-21_SE3.json";
+    public PricePerHour[] fetch(LocalDate date, String priceBracket) throws IOException, InterruptedException {
+
+        String baseUrl = "https://www.elprisetjustnu.se/api/v1/prices/";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM-dd");
+        String dateString = date.format(formatter) + "_" + priceBracket + ".json";
+
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(baseUrl + dateString))
+                .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String body = response.body();
 
-        JsonElement jsonElement = JsonParser.parseString(response.body());
+        Gson gson = new Gson();
+        if(body.trim().startsWith("[")){
+            return gson.fromJson(response.body(), PricePerHour[].class);
+        }
+        else{
+            System.out.println("Ingen data tillgänglig för " + date + " i " + priceBracket);
+            return new PricePerHour[0]; // return empty array instead of crashing if there is no available prices.
+        }
 
-        String prettyJsonString = gson.toJson(jsonElement);
-
-        System.out.println(prettyJsonString);
     }
 }
---
+++ src/main/java/org/example/PricePerHour.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public record PricePerHour(
+        double SEK_per_kWh,
+        double EUR_per_kWh,
+        String EXR,
+        String time_start,
+        String time_end
+
+) { }

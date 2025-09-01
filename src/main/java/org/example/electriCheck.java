f14803e095f788d04ddf872ab2807365b6377f32
RobTrb <Robert.trb@gmail.com>
2025-09-01 22:56:18 +0200

    Prints out cheapest and most expensive hours and the average price of the 24 hour period

--- src/main/java/org/example/electriCheck.java
+++ src/main/java/org/example/electriCheck.java
@@ -6,8 +6,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpResponse;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 import java.time.LocalDate;
 import java.net.http.HttpClient;
@@ -19,8 +22,7 @@ public class electriCheck {
     public static String zone = "";
     public static String date = formatedDate();
 
-
-    public static void main(String[] args) {
+    static void main() {
 
         System.out.println("Welcome to ElectriCheck \n This is a simple electricity price checking software. \n This program will download the electricity prices for the upcoming 24 hours \n (note that upcoming prices are released after 1 PM so checking before that will not take the full 24 hours into account) \n");
         System.out.println("Please input your price zone? \n \n SE1 = Luleå / Norra Sverige \n SE2 = Sundsvall / Norra Mellansverige \n SE3 = Stockholm / Södra Mellansverige \n SE4 = Malmö / Södra Sverige \n");
@@ -35,10 +37,44 @@ public class electriCheck {
             }
         } while (!zone.equalsIgnoreCase("SE1") && !zone.equalsIgnoreCase("SE2") && !zone.equalsIgnoreCase("SE3") && !zone.equalsIgnoreCase("SE4"));
 
-        System.out.println("Loading current Electricity prices...");
+        System.out.println("Loading current Electricity prices... \n");
+
         ArrayList<priceObject> prices = getPrices(zone, date);
-        System.out.println("sucess:" + prices);
-        System.out.println(date + " " + zone);
+        double medianPrice = getMedianPrice(prices);
+        ArrayList<priceObject> lowHighPrices = getLowHighPrices(prices);
+
+        System.out.println("The time and price of the cheapest hours:\n");
+        DateTimeFormatter time = DateTimeFormatter.ofPattern("HH:mm");
+        DateTimeFormatter apiDateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
+        int counter = 0;
+        for (int i = 0; i <= 2; i++) {
+            priceObject currentObject = lowHighPrices.get(i);
+
+            LocalDateTime startTime = LocalDateTime.parse(currentObject.time_start, apiDateTimeFormatter);
+            LocalDateTime endTime = LocalDateTime.parse(currentObject.time_end, apiDateTimeFormatter);
+
+            counter++;
+            String formatedStartTime = startTime.format(time);
+            String formatedEndTime = endTime.format(time);
+            System.out.println(counter + ".Time: " + formatedStartTime + " - " + formatedEndTime + " Price: " + currentObject.SEK_per_kWh + " SEK");
+        }
+        counter = 0;
+        System.out.println("\nThe time and price of the most expensive hours: \n");
+        for (int i = (lowHighPrices.size() - 1); i >= (lowHighPrices.size() - 3); i--) {
+            priceObject currentObject = lowHighPrices.get(i);
+
+            LocalDateTime startTime = LocalDateTime.parse(currentObject.time_start, apiDateTimeFormatter);
+            LocalDateTime endTime = LocalDateTime.parse(currentObject.time_end, apiDateTimeFormatter);
+
+            counter++;
+            String formatedStartTime = startTime.format(time);
+            String formatedEndTime = endTime.format(time);
+            System.out.println(counter + ".Time: " + formatedStartTime + " - " + formatedEndTime + " Price: " + currentObject.SEK_per_kWh + " SEK");
+        }
+
+
+        System.out.println("\nAverage price for 24 hour period: " + medianPrice + " SEK");
+
     }
 
     public static String formatedDate() {
@@ -86,6 +122,28 @@ public class electriCheck {
         public double EXR;
         public String time_start;
         public String time_end;
+    }
+
+    public static double getMedianPrice(ArrayList<priceObject> prices) {
+        double medianPrice = 0;
+        for (priceObject price : prices) {
+            double eachPrice = price.SEK_per_kWh;
+            medianPrice += eachPrice;
+        }
+        medianPrice /= prices.size();
+        return medianPrice;
+    }
+
+    public static ArrayList<priceObject> getLowHighPrices(ArrayList<priceObject> prices) {
+        ArrayList<priceObject> lowHighPrices = new ArrayList<>();
+        prices.sort((obj1, obj2) -> Double.compare(obj1.SEK_per_kWh, obj2.SEK_per_kWh));
+        for (int i = 0; i <= 2; i++) {
+            lowHighPrices.add(prices.get(i));
+        }
+        for (int i = (prices.size() - 3); i <= (prices.size() - 1); i++) {
+            lowHighPrices.add(prices.get(i));
+        }
+        return lowHighPrices;
     }
 
 }

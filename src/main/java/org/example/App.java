d2d10700830a6036bbe7d332d248a0768e37e957
Alfred Brännare <alfred-brannare@hotmail.com>
2025-09-01 12:27:48 +0200

    Implement initial idea of an Inventory.java.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,23 @@
 package org.example;
 
+import org.example.entities.characters.Player;
 import org.example.entities.items.Inventory;
 import org.example.entities.items.Item;
+import org.example.entities.items.Weapon;
+import org.example.entities.items.potions.HealthPotion;
+import org.example.map.DungeonGrid;
+import org.example.map.Tile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
-Inventory inventory = new Inventory;
+    public static void main(String[] args) {
+        List<Item> starterItems = new ArrayList<>();
+        starterItems.add(new HealthPotion("Minor Health Potion", "Restores health", 10, 20));
+        starterItems.add(new Weapon("The Spork of Indecision", "It can't decide if it wants to stab or scoop, so it does a little of both, poorly.", 3, 20));
 
+        Inventory inventory = new Inventory(starterItems);
+
+    }
 }

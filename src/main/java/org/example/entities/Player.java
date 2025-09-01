364e059a98aadeb856185a6ade9280c356603fad
jennymakki <jjennyeklundd@hotmail.com>
2025-09-01 20:10:33 +0200

    Made sure the player can see how much damage they will inflict and how much health they have after every turn. And made sure that the items that are picked up will increase damage.

--- src/main/java/org/example/entities/Player.java
+++ src/main/java/org/example/entities/Player.java
@@ -1,17 +1,31 @@
 package org.example.entities;
 
+
 public class Player extends Character {
     private Inventory inventory;
+    private Weapon equippedWeapon;
 
     public Player(String name, int maxHP, int baseDamage, Position position) {
         super(name, maxHP, baseDamage, position);
         this.inventory = new Inventory();
     }
 
+    public int getDamage() {
+        int bonus = (equippedWeapon != null) ? equippedWeapon.getDamageBonus() : 0;
+        return getBaseDamage() + bonus;
+    }
+
     public void attack(Enemy enemy) {
-        System.out.println(name + " attacks " + enemy.getName() + " for " + baseDamage + " damage!");
-        enemy.takeDamage(baseDamage);
+        int damage = getDamage();
+        System.out.println(name + " attacks " + enemy.getName() + " for " + damage + " damage!" +
+                (equippedWeapon != null ? " (" + getBaseDamage() + " + " + equippedWeapon.getDamageBonus() + ")" : ""));
+        enemy.takeDamage(damage);
         System.out.println(enemy.getName() + " now has " + enemy.getHp() + " HP left.");
+    }
+
+    public void equipWeapon(Weapon weapon) {
+        this.equippedWeapon = weapon;
+        System.out.println(name + " equips " + weapon.getName() + " (+" + weapon.getDamageBonus() + " damage)!");
     }
 
     public void pickUpItem(Item item) {
@@ -19,11 +33,12 @@ public class Player extends Character {
     }
 
     public void useItem(int index) {
-        inventory.useItem(index, this);
-    }
-
-    public void showInventory() {
-        inventory.showItems();
+        Item item = inventory.getItem(index);
+        if (item instanceof Weapon weapon) {
+            equipWeapon(weapon);
+        } else {
+            inventory.useItem(index, this);
+        }
     }
 
     public Inventory getInventory() {

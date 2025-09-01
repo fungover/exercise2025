42495ffa3e0dbd37793f22901e64852d9ceb13cc
Henrik <Henrikmattsson89@gmail.com>
2025-09-01 14:12:13 +0200

    FIX: if case ex. "w or west" we now assign "West" to fullDirection that we send to our print.

--- src/main/java/org/example/service/MovementService.java
+++ src/main/java/org/example/service/MovementService.java
@@ -5,36 +5,39 @@ import org.example.map.DungeonGenerator;
 
 public class MovementService {
 
-    public static boolean movePlayer(Player player, String direction,
-                                     DungeonGenerator dungeon) {
+    public static boolean movePlayer(Player player, String direction, DungeonGenerator dungeon) {
         int newX = player.getX();
         int newY = player.getY();
+        String fullDirection;
 
         switch (direction.toLowerCase()) {
             case "north":
             case "n":
                 newY--;
+                fullDirection = "North";
                 break;
             case "south":
             case "s":
                 newY++;
+                fullDirection = "South";
                 break;
             case "east":
             case "e":
                 newX++;
+                fullDirection = "East";
                 break;
             case "west":
             case "w":
                 newX--;
+                fullDirection = "West";
                 break;
             default:
-                System.out.println(
-                  "Invalid direction! Use: north, south, east, west (or n, s, e, w");
+                System.out.println("Invalid direction! Use: north, south, east, west (or n, s, e, w");
                 return false;
         }
         if (dungeon.isValidPosition(newX, newY)) {
             player.setPosition(newX, newY);
-            System.out.println("You moved " + direction + ".");
+            System.out.println("You moved " + fullDirection + ".");
             return true;
         } else {
             System.out.println("You can't move there! There's a wall in the way.");

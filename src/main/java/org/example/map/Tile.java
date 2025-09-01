a6006a0e739d9ea72f95b71f0d10e6c54391fff7
Alfred Brännare <alfred-brannare@hotmail.com>
2025-09-01 14:08:42 +0200

    Implement blocked, explored, hasPlayer, enemy and items fields in Tile.java

--- src/main/java/org/example/map/Tile.java
+++ src/main/java/org/example/map/Tile.java
@@ -1,7 +1,17 @@
 package org.example.map;
 
+import org.example.entities.enemies.Enemy;
+import org.example.entities.items.Item;
+
+import java.util.List;
+
 public class Tile {
     private TileType type;
+    private boolean blocked;
+    private boolean explored;
+    private boolean hasPlayer;
+    private Enemy enemy;
+    private List<Item> items;
 
     public enum TileType {
         WALL,
@@ -20,8 +30,48 @@ public class Tile {
         return type;
     }
 
+    public boolean isBlocked() {
+        return blocked;
+    }
+
+    public boolean isExplored() {
+        return explored;
+    }
+
+    public boolean isHasPlayer() {
+        return hasPlayer;
+    }
+
+    public Enemy getEnemy() {
+        return enemy;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
     //Setters
     public void setType(TileType type) {
         this.type = type;
+    }
+
+    public void setBlocked(boolean blocked) {
+        this.blocked = blocked;
+    }
+
+    public void setExplored(boolean explored) {
+        this.explored = explored;
+    }
+
+    public void setHasPlayer(boolean hasPlayer) {
+        this.hasPlayer = hasPlayer;
+    }
+
+    public void setEnemy(Enemy enemy) {
+        this.enemy = enemy;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
     }
 }

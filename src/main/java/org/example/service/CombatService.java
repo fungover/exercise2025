218e00e0ab97dc90b80dda0d90a84669b71ebb21
Emilyempa <emilypettersson@hotmail.com>
2025-09-01 14:20:56 +0200

    Remove comment

--- src/main/java/org/example/service/CombatService.java
+++ src/main/java/org/example/service/CombatService.java
@@ -9,7 +9,7 @@ import org.example.map.Tile;
 public class CombatService {
     public static void attack(Player player, FarmageddonMap map) {
         Tile tile = map.getTile(player.getX(), player.getY());
-        Hostile hostile = tile.getEnemy(); // getEnemy() returnerar en Enemy, som är Hostile
+        Hostile hostile = tile.getEnemy();
 
         if (tile.getType() == Tile.Type.ENEMY && hostile != null) {
             hostile.attack(player);

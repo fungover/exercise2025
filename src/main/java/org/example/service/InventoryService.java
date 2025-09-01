afa37e987708b1f3c6e08d0d1c9d3220038e9f4b
Emilyempa <emilypettersson@hotmail.com>
2025-09-01 13:42:57 +0200

    Minor output change

--- src/main/java/org/example/service/InventoryService.java
+++ src/main/java/org/example/service/InventoryService.java
@@ -6,7 +6,7 @@ public class InventoryService {
     public static void showInventory(Player player) {
         System.out.println("Inventory:");
         if (player.getInventory().isEmpty()) {
-            System.out.println("- (your inventory is empty)");
+            System.out.println("- your inventory is empty");
         } else {
             player.getInventory().forEach(item -> System.out.println("- " + item.getName()));
         }

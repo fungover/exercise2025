3d7653dd0c88c88ce2091d528723c14ef546acc9
Deansie <dennis-andersen@outlook.com>
2025-09-02 21:33:38 +0200

    feat(Tile): init setup of fields and constructors for tiles

--- src/main/java/org/example/entities/Tile.java
+++ src/main/java/org/example/entities/Tile.java
@@ -1,4 +1,40 @@
 package org.example.entities;
 
 public class Tile {
+
+    private TileType type;
+    private Enemy enemy;
+    private Item item;
+
+    public Tile(TileType type) {
+        if (type == null) {
+            throw new IllegalArgumentException("Tile type cannot be null");
+        }
+        this.type = type;
+        this.enemy = null;
+        this.item = null;
+
+    }
+
+    public Tile(Enemy enemy) {
+        if (enemy == null) {
+            throw new IllegalArgumentException("Enemy cannot be null");
+        }
+        this.type = TileType.ENEMY;
+        this.enemy = enemy;
+        this.item = null;
+    }
+
+    public Tile(Item item) {
+        if (item == null) {
+        throw new IllegalArgumentException("Item cannot be null");
+        }
+        this.type = TileType.ITEM;
+        this.enemy = null;
+        this.item = item;
+    }
+
+    // Get the tiles
+
 }
+

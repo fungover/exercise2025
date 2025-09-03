81f09f9d9e0772a804d390eb0b7c42415c9f88fc
Louise <emmalouisekarlsson@hotmail.com>
2025-09-03 10:17:09 +0200

    Bug fix

--- src/main/java/ExerciseTwo/Service/Position.java
+++ src/main/java/ExerciseTwo/Service/Position.java
@@ -32,7 +32,7 @@ public class Position {
     }
 
     public void setPosition(int row,  int col) {
-        this.row += row;
-        this.col += col;
+        this.row = row;
+        this.col = col;
     }
 }

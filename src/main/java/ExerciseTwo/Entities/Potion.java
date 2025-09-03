ec7aeb8dfc9db0d7db82ff796b496c948260f729
Louise <emmalouisekarlsson@hotmail.com>
2025-09-03 14:47:21 +0200

    Adjusted Item class added subclass for potion and coin. Also added tile for potion and coin.

---
+++ src/main/java/ExerciseTwo/Entities/Potion.java
@@ -0,0 +1,13 @@
+package ExerciseTwo.Entities;
+
+public class Potion extends Item{
+
+    public Potion() {
+        super("Potion", 25);
+    }
+
+    @Override
+    public void itemFound() {
+        System.out.println(type+" found. restores "+effect+" hp if used");
+    }
+}
--- src/main/java/ExerciseTwo/Service/Emojis.java
+++ src/main/java/ExerciseTwo/Service/Emojis.java
@@ -5,9 +5,10 @@ public class Emojis {
     public static final String wall = "\uD83E\uDDF1";
     public static final String path = "⬜";
     public static final String enemy = "\uD83D\uDE08";
+    public static final String potion = "\uD83E\uDED9";
     public static final String skull = "\uD83D\uDC80";
     public static final String player = "\uD83E\uDDCD";
-    public static final String gift = "\uD83C\uDF81";
+    public static final String gift = "\uD83E\uDE99";
     public static final String treasure = "\uD83D\uDC8E";
     public static final String door = "\uD83D\uDEAA";
 }

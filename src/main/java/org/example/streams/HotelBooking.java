35fd8554297f71f3fa72a88a2c64ab5279a62928
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-03 12:53:39 +0200

    Modify Bookings

--- src/main/java/org/example/streams/HotelBooking.java
+++ src/main/java/org/example/streams/HotelBooking.java
@@ -3,4 +3,8 @@ package org.example.streams;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record HotelBooking(Guest guest, String roomType, LocalDate checkInDate, LocalDate checkOutDate, BigDecimal pricePerNight) {}
+public record HotelBooking(Guest guest,
+                           String roomType,
+                           LocalDate checkInDate,
+                           LocalDate checkOutDate,
+                           BigDecimal pricePerNight) {}

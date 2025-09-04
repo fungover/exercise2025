8050397fedd38ef35b46c0c6534da1f8dab1f3ef
Rikard <rikardengstrom89@gmail.com>
2025-09-04 14:18:11 +0200

    Added first successfull test on addProduct method.

--- src/test/java/org/example/service/WarehouseTest.java
+++ src/test/java/org/example/service/WarehouseTest.java
@@ -1,7 +1,15 @@
 package org.example.service;
 
+import org.example.entities.Category;
 import org.example.entities.Product;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class WarehouseTest {
 
@@ -11,6 +19,22 @@ class WarehouseTest {
     @BeforeEach
     void setUp() {
         warehouse = new Warehouse();
-        
+
+    }
+
+    // ========================================
+    // TESTS FOR addProduct(Product product)
+    // ========================================
+
+    @Test
+    @DisplayName("Should add product successfully")
+    void addProductSuccessfully() {
+
+        Product testProduct = Warehouse.createProduct("1", "Test Product", Category.ELECTRONICS, 8);
+        warehouse.addProduct(testProduct);
+
+        List<Product> products = warehouse.getAllProducts();
+        assertEquals(1, products.size());
+        assertTrue(products.contains(testProduct));
     }
 }

5f6bbb4acc4c91dabe13d3ad5c06a75de4106104
Rikard <rikardengstrom89@gmail.com>
2025-09-04 14:09:12 +0200

    Setup file for testing

---
+++ src/test/java/org/example/service/WarehouseTest.java
@@ -0,0 +1,16 @@
+package org.example.service;
+
+import org.example.entities.Product;
+import org.junit.jupiter.api.BeforeEach;
+
+class WarehouseTest {
+
+    private Warehouse warehouse;
+    private Product product;
+
+    @BeforeEach
+    void setUp() {
+        warehouse = new Warehouse();
+        
+    }
+}

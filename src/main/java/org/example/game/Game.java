4a41c3155ae195a7ecca7990ff9f6484f1867100
Taru Keskinen <taru.keskinen@lerniastudent.se>
2025-09-04 13:25:57 +0200

    Add player methods for name and health

--- src/main/java/org/example/game/Game.java
+++ src/main/java/org/example/game/Game.java
@@ -1,14 +1,19 @@
 package org.example.game;
 
+import org.example.entities.Player;
 import java.util.Scanner;
 
 public class Game {
 private Scanner scanner = new Scanner(System.in);
+private Player player;
 
     public void startGame() {
     System.out.println("Welcome to the Dungeon.... I have been waiting for an adventurer..");
     System.out.print("Please, tell me your name ");
-        String name = scanner.nextLine();
-    System.out.print("Well Hello, " + name + ". Step inside and see what happens...");
-}
+    String name = scanner.nextLine();
+
+    player = new Player(name);
+    System.out.println("Well Hello, " + player.getName() + ". Step inside and see what happens...");
+    System.out.println("Your health is " + player.getHealth());
+    }
 }

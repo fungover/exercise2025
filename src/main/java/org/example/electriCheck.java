4ee42da5457d4d3e9d2a597c0ff454356d92045a
RobTrb <Robert.trb@gmail.com>
2025-09-04 10:17:19 +0200

    removed unused variable and made minor fixes

--- src/main/java/org/example/electriCheck.java
+++ src/main/java/org/example/electriCheck.java
@@ -22,7 +22,7 @@ public class electriCheck {
     public static DateTimeFormatter time = DateTimeFormatter.ofPattern("HH:mm");
     public static DateTimeFormatter apiDateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
 
-    static void main() {
+    static void main(String[] args) {
 
         System.out.println("Welcome to ElectriCheck \n This is a simple electricity price checking software. \n This program will download the electricity prices for the upcoming 24 hours \n (note that upcoming prices are released after 1 PM so checking before that will not take the full 24 hours into account) \n");
         System.out.println("Please input your price zone? \n \n SE1 = Luleå / Norra Sverige \n SE2 = Sundsvall / Norra Mellansverige \n SE3 = Stockholm / Södra Mellansverige \n SE4 = Malmö / Södra Sverige \n");
@@ -162,7 +162,6 @@ public class electriCheck {
         double lowestPrice = Double.MAX_VALUE;
         int startIndex = 0;
         double currentWindowPrice = 0;
-        String startTime = "";
 
         for(int i = startIndex; i < chargeInterval; i++){
             currentWindowPrice += prices.get(i).SEK_per_kWh;

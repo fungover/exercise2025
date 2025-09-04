7bac430060bf9cd406041192943de31181e25910
Calle <ca.karlsson@hotmail.com>
2025-09-04 12:47:48 +0200

    Added first version of item class with basic values and get & set methods

---
+++ src/main/java/org/game/entities/Item.java
@@ -0,0 +1,18 @@
+package org.game.entities;
+
+public class Item {
+    private String name;
+    private String type;
+    private int price;
+
+    public Item(String name, String type, int price) {
+        this.name = name;
+        this.type = type;
+        this.price = price;
+    }
+
+    public String getName() {return name;}
+    public String getType() {return type;}
+    public int getPrice() {return price;}
+    public void setName(String name) {this.name = name;}
+}

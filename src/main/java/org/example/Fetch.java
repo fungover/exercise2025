e15fba9715b31c5e7d5db28ccf4dc4bed8e3bdf4
Patseroni <patrik.bustad@lerniastudent.se>
2025-09-04 08:23:22 +0200

    HTTP-code update according to coderabbit

--- src/main/java/org/example/Fetch.java
+++ src/main/java/org/example/Fetch.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import com.google.gson.Gson;
@@ -17,11 +18,21 @@ class Fetch {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM-dd");
         String dateString = date.format(formatter) + "_" + priceZone + ".json";
 
-        HttpClient client = HttpClient.newHttpClient();
+
+        HttpClient client = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(5))
+                .build();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(baseUrl + dateString))
+                .timeout(Duration.ofSeconds(10))
+                .GET()
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != 200) {
+            System.out.println("\nIngen data tillgänglig för " + date + " i " + priceZone + " (HTTP " + response.statusCode() + ")\n");
+            return new PricePerHour[0];
+        }
+
 
         String body = response.body();
 

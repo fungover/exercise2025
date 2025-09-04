90daa31e6cc727c2de150baf7d99d466ef3d6a62
SpinalGlitter <johannes.persson47@gmail.com>
2025-09-04 12:14:03 +0200

    Did some tests for position/movement logic and then started to do combat tests.
    Co-authored-by: Empyreanmist <christianfryksten1@gmail.com>
    Co-authored-by: marcusmoller97 <marcusssmoller@gmail.com>

--- src/main/java/org/SpinalGlitter/exercise2/entities/Position.java
+++ src/main/java/org/SpinalGlitter/exercise2/entities/Position.java
@@ -2,4 +2,6 @@ package org.SpinalGlitter.exercise2.entities;
 
 public record Position(int x, int y) {
     public Position getAdjacent(int dx, int dy) {
-        return new Position(this.x + dx, this.y + dy); } }
+        return new Position(this.x + dx, this.y + dy);
+    }
+}

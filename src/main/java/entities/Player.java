8e3f3d5e92bdc31d3d3a4844965664bcb43881ef
0x0xoscar <139160108+0x0xoscar@users.noreply.github.com.>
2025-09-05 10:35:59 +0200

    Added more methods to Player

--- src/main/java/entities/Player.java
+++ src/main/java/entities/Player.java
@@ -8,7 +8,8 @@ public class Player {
     private String name = "Unknown Slayer";
     private int health = 100;
     private Position position;
-    private ArrayList<Item> inventory = new ArrayList<>()
+    private ArrayList<Item> inventory = new ArrayList<>();
+    private Item currentItem;
 
     public boolean isAlive() {
         return health > 0;
@@ -23,8 +24,31 @@ public class Player {
         }
     }
 
+    public void move(int dx, int dy) {
+        position.move(dx, dy);
+    }
+
+    public void addItem(Item item) {
+        inventory.add(item);
+        System.out.printf("You just picked up " + item);
+    }
+
+    public void useItem(Item item) {
+        currentItem = item;
+    }
+
+    public void listInventory() {
+        if (inventory.isEmpty()) {
+            System.out.println("You have no items");
+        } else {
+            for (Item item : inventory) {
+                System.out.println(item);
+            }
+        }
+    }
+
     public void attack(Enemy enemy) {
-        //TODO
+        // TODO
     }
 
 }

03dcb6fcf1f5fe133b35654d3e3a0d74e7cb75e7
NordicNomadicLife <Pelle.nilsson.93@hotmail.com>
2025-09-05 14:36:35 +0200

    i have made an character and have fixed so he can use weapons and potions

--- src/main/java/Entities/Character.java
+++ src/main/java/Entities/Character.java
@@ -1,26 +1,49 @@
 package Entities;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
+import Entities.Player;
 public abstract class Character {
-    private final UUID id;
-    protected String type;
+    protected String name;
     protected int health;
     protected int attack;
     protected int x;
     protected int y;
 
-    private static final List<Character> CHARACTERS = new ArrayList<>();
-
-    public Character(String type, int health, int attack, int x, int y) {
-        this.id = UUID.randomUUID();
-        this.type = type;
+    public Character(String name, int health, int attack, int x, int y) {
+        this.name = name;
         this.health = health;
         this.attack = attack;
         this.x = x;
         this.y = y;
-        CHARACTERS.add(this);
+    }
+
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    public void takeDamage(int dmg) {
+        health -= dmg;
+        if (health < 0) health = 0;
+    }
+
+    public void attack(Character target) {
+        int dmg = getAttackPower();
+        System.out.println(name + " attacks " + target.getName() + " for " + dmg + "!");
+        target.takeDamage(dmg);
+    }
+
+    public int getAttackPower() {
+        return this.attack;
+    }
+
+
+    public String getName() { return name; }
+    public int getHealth() { return health; }
+    public int getAttack() { return attack; }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public void setPos(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }

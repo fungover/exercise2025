d2d7a2ef8950deb5d58c3c5f40a545a98d005567
0x0xoscar <139160108+0x0xoscar@users.noreply.github.com.>
2025-09-05 10:39:54 +0200

    Added Item class

---
+++ src/main/java/entities/Item.java
@@ -0,0 +1,26 @@
+package entities;
+
+public abstract class Item {
+    private String name;
+    private String type;
+    private int effect;
+
+    public Item(String name, String type, int effectValue) {
+        this.name = name;
+        this.type = type;
+        this.effect = effectValue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getEffect() {
+        return effect;
+    }
+
+}

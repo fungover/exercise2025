ed5fe79b821af727deec33589d35b3e8434cfe20
Emilyempa <emilypettersson@hotmail.com>
2025-09-05 15:40:53 +0200

    Refactor look to be more informative

--- src/main/java/org/example/service/MapService.java
+++ src/main/java/org/example/service/MapService.java
@@ -6,7 +6,13 @@ import org.example.map.Tile;
 
 public class MapService {
     public void look(Player player, FarmageddonMap map) {
-        Tile tile = map.getTile(player.getX(), player.getY());
-        System.out.println("You are standing on a " + tile.getType());
+        int x = player.getX();
+        int y = player.getY();
+        if (x < 0 || y < 0 || x >= map.getWidth() || y >= map.getHeight()) {
+            System.out.println("Watch out! You are at the edge of the map.");
+            return;
+        }
+        Tile tile = map.getTile(x, y);
+        System.out.println("You are standing on " + tile.getType().name().toLowerCase());
     }
 }

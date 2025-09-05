9cc4aa9de4e2b82d53d5fb166f9ac552d822e049
Patseroni <patrik.bustad@lerniastudent.se>
2025-09-05 14:04:55 +0200

    Inital commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,38 +1,7 @@
 package org.example;
 
-import java.io.IOException;
-import java.time.LocalDate;
-
 public class App {
-
-    public static void main(String[] args) throws IOException {
-
-        try {
-            String priceZone = Menu.chooseArea();
-
-            PricePerHour[] today = Fetch.fetch(LocalDate.now(), priceZone);
-            PricePerHour[] tomorrow = Fetch.fetch(LocalDate.now().plusDays(1), priceZone);
-
-            if(today.length > 0){
-                System.out.print("\n---- Dagens priser ---- ");
-                Printer.printCalculatedPrices(today);
-                Printer.printBestChargingPeriods(today);
-            }
-
-            System.out.println();
-
-            if(tomorrow.length > 0){
-                System.out.print("---- Morgondagens priser ----");
-                Printer.printCalculatedPrices(tomorrow);
-                Printer.printBestChargingPeriods(tomorrow);
-            }
-
-
-        } catch (IOException | InterruptedException e) {
-            System.err.println("Fel: " + e.getMessage());
-        }
-
-
+    static void main(String[] args) {
+        
     }
 }
-

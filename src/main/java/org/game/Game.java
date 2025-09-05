8ce00f4a3e7ee4a69c435a7a8582924df4e16cba
Calle <ca.karlsson@hotmail.com>
2025-09-05 13:39:36 +0200

    Added command to test combat and stackable items in the game loop

--- src/main/java/org/game/Game.java
+++ src/main/java/org/game/Game.java
@@ -3,8 +3,9 @@ package org.game;
 import org.game.entities.Item;
 import org.game.entities.Player;
 import org.game.entities.npcs.Goblin;
+import org.game.systems.CombatHandler;
 import org.game.systems.InputHandler;
-
+import org.game.utils.ClearConsole;
 
 
 public class Game {
@@ -39,14 +40,15 @@ public class Game {
 
                     // Todo dev commands
                 case "die":
-                    player.TakeDamage(1000);
+                    player.takeDamage(1000,true);
                     break;
 
                 case "additem":
                     String itemName = InputHandler.getInput("Enter item name: ");
                     String ItemType = InputHandler.getInput("Type of item: ");
                     int itemValue = Integer.parseInt(InputHandler.getInput("Enter value of item: "));
-                    Item item = new Item(itemName,ItemType,itemValue);
+                    int itemQuantity = Integer.parseInt(InputHandler.getInput("Enter value of item quantity: "));
+                    Item item = new Item(itemName,ItemType,itemValue, itemQuantity);
                     player.addItem(item);
                     break;
 
@@ -54,23 +56,36 @@ public class Game {
 
                 case "takedamage":
                     String takeDamage = InputHandler.getInput("Enter damage amount: ");
-                    player.TakeDamage(Integer.parseInt(takeDamage));
+                    player.takeDamage(Integer.parseInt(takeDamage),false);
                     System.out.println("you have "+ player.getHealth() + " hp left");
                     break;
 
                 case "spawnenemy":
-                    System.out.println("Spawning Goblin");
-                    Goblin testGoblin = new Goblin(0,0);
-                    testGoblin.setHealth(0);
-                    System.out.println("you have defeated the Goblin");
+                    for (int i =0; i < 1000; i++) {
+                        //System.out.println("Spawning Goblin");
+                        Goblin testGoblin = new Goblin(0, 0);
+                        testGoblin.setHealth(0);
+                        //System.out.println("you have defeated the Goblin");
 
-                    Item loot = testGoblin.getLoot();
-                    if (loot != null) {
-                        System.out.println("The Goblin dropped " + loot.getName());
-                        player.addItem(loot);
-                    }else {
-                        System.out.println("The Goblin dropped nothing");
+                        Item loots = testGoblin.getLoot();
+                        if (loots != null) {
+                           // System.out.println("The Goblin dropped " + loots.getName()+ " x " + loots.getQuantity());
+                            player.addItem(loots);
+                        } else {
+                          //  System.out.println("The Goblin dropped nothing");
+                        }
                     }
+                    break;
+
+                case "fight":
+                    ClearConsole.clearConsole();
+                    Goblin goblin = new Goblin(0,0);
+                    System.out.println("Fighting a " + goblin.getName());
+                    CombatHandler.fight(player, goblin);
+                    break;
+
+                case "loot":
+                    player.addItem(new Item("Healing Potion","Potion",10,100));
                     break;
 
                 default:

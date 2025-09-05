53daa3f78a0cd008ee929baf40b11d9f129192c2
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-05 09:30:00 +0200

    createDogTestAndClass

---
+++ src/test/java/org/example/tdd/DogTest.java
@@ -0,0 +1,15 @@
+package org.example.tdd;
+
+import org.junit.jupiter.api.Test;
+
+public class DogTest {
+
+    @Test
+    public void canCreateDogObject() {
+        Dog dog = new Dog();
+    }
+
+    
+
+
+}

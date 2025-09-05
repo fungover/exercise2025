4b5d59707bc6b7d31b363558937b1fceefbfa08e
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-05 11:39:33 +0200

    Throw exception when product not in cart

--- src/test/java/org/example/tdd/ShoppingCartTest.java
+++ src/test/java/org/example/tdd/ShoppingCartTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ShoppingCartTest {
     ShoppingCart shoppingCart = new ShoppingCart();

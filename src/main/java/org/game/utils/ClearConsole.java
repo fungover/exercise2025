fe862c4ef58729bb329abc1f3aee0c9554992f2f
Calle <ca.karlsson@hotmail.com>
2025-09-05 13:34:38 +0200

    Added chat utils for use in combat and other actions

---
+++ src/main/java/org/game/utils/ClearConsole.java
@@ -0,0 +1,9 @@
+package org.game.utils;
+
+public class ClearConsole {
+    public static void clearConsole(){
+        for (int i=0; i < 50; i++){
+            System.out.println("");
+        }
+    }
+}
---
+++ src/main/java/org/game/utils/Colors.java
@@ -0,0 +1,9 @@
+package org.game.utils;
+
+public class Colors {
+    public static final String resetColor = "\u001B[0m";
+    public static final String redColor = "\u001B[31m";
+    public static final String yellowColor = "\u001B[33m";
+    public static final String greenColor = "\u001B[32m";
+    public static final String cyanColor = "\u001B[36m";
+}

b33c57aa8e6bb0d9c86367cdce05efe00ab88357
Deansie <dennis-andersen@outlook.com>
2025-09-06 07:34:36 +0200

    feat(Dungeon): add interface for MapGenerator - add fields in constructor

--- src/main/java/org/example/map/Dungeon.java
+++ src/main/java/org/example/map/Dungeon.java
@@ -1,4 +1,15 @@
 package org.example.map;
 
+import org.example.entities.*;
+
+interface MapGenerator {
+    Tile[][] generate (int width, int height);
+}
+
 public class Dungeon {
+    private final int width;
+    private final int height;
+    private final Tile[][] tiles;
+    private final Player player;
+    private final MapGenerator mapGenerator;
 }

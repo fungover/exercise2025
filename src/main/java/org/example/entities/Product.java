1a36af9558718417cf3810e7247445f275fc57ff
Alfred Brännare <alfred-brannare@hotmail.com>
2025-09-07 12:52:00 +0200

    feat: implement Product constructors and getters to satisfy tests

--- src/main/java/org/example/entities/Product.java
+++ src/main/java/org/example/entities/Product.java
@@ -2,12 +2,23 @@ package org.example.entities;
 
 public class Product {
     private int id;
+    private String name;
 
     public Product(int id) {
         this.id = id;
+        this.name = "";
+    }
+
+    public Product(int id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public int getId() {
         return this.id;
+    }
+
+    public String getName() {
+        return this.name;
     }
 }
--- src/test/java/org/example/ProductTest.java
+++ src/test/java/org/example/ProductTest.java
@@ -14,4 +14,12 @@ public class ProductTest {
         Product product = new Product(1);
         assertThat(product.getId()).isEqualTo(1);
     }
+
+
+    @Test
+    @DisplayName("Can create a Product with a name")
+    public void canCreateProductWithName(){
+        Product product = new Product(1, "Laptop");
+        assertThat(product.getName()).isEqualTo("Laptop");
+    }
 }

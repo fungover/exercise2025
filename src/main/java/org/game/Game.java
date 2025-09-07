8f4b1ecd84d07e3d8d0d5bc051ffba597cce4aff
Calle <ca.karlsson@hotmail.com>
2025-09-07 17:23:50 +0200

    Updated gameloop with new added features

--- src/main/java/org/game/Game.java
+++ src/main/java/org/game/Game.java
@@ -2,6 +2,7 @@ package org.game;
 
 import java.util.Collections;
 import java.util.List;
+import org.game.entities.Enemy;
 import org.game.entities.Item;
 import org.game.entities.Player;
 import org.game.entities.npcs.Goblin;
@@ -9,7 +10,9 @@ import org.game.systems.CombatHandler;
 import org.game.systems.InputHandler;
 import org.game.utils.ClearConsole;
 import org.game.utils.Colors;
-
+import org.game.world.Dungeon;
+import org.game.world.Room;
+import org.game.world.Tile;
 
 
 public class Game {
@@ -17,22 +20,66 @@ public class Game {
         System.out.println("Welcome to the Dungeon Game!");
 
         //Todo Name and starting cords
-        Player player = new Player("Hero",0,0);
-
+        Player player = new Player("Hero",1,1);
+        Dungeon dungeon = new Dungeon(10,10);
         boolean running = true;
 
         while (running && player.isAlive()) {
-            String input = InputHandler.getInput(Colors.yellowColor +
-                    "Valid commands: move | inventory | equip | equiptment | die | additem | takedamage | fight | spawnloot | quit"
-                    + Colors.resetColor);
+           // dungeon.printDungeon(player.getX(), player.getY());
+
+            System.out.println(Colors.blueColor + "Dev commands: die | additems | takedamage | spawnloot | loot | fight " + Colors.resetColor);
+            String input = InputHandler.getInput("%sValid commands: move | inventory | equip | equiptment | quit%s".formatted(Colors.yellowColor, Colors.resetColor));
+
 
             switch (input) {
                 case "move":
                     String dir = InputHandler.getInput("Direction (north/south/east/west)");
-                    System.out.println("Direction: " + dir);
+                    int newX = player.getX();
+                    int newY = player.getY();
+
+                    switch (dir.toLowerCase()){
+                        case "north": newY--; break;
+                        case "south": newY++; break;
+                        case "east": newX++; break;
+                        case "west": newX--; break;
+                        default:
+                            System.out.println("Invalid direction");
+                            break;
+                    }
+
+                    if (dungeon.isWalkable(newX, newY)) {
+                        player.setPosition(newX,newY);
+
+                        Tile tile = dungeon.getTile(player.getX(), player.getY());
+                        if (tile != null && tile.getRoom() != null && !tile.getRoom().IsVisited()) {
+                            Room room = tile.getRoom();
+                            System.out.println("You encountered a " + room.getType() + "room!");
+
+                            for (Enemy e: room.getEnemies()) {
+                                CombatHandler.fight(player,e);
+                            }
+
+                            for (Item loot : room.getLoot()) {
+                                player.addItem(loot);
+                            }
+
+                            room.setVisited(true);
+
+                        }
+
+                        System.out.println("You moved " + dir);
+                        System.out.println("Press enter to continue...");
+                        new java.util.Scanner(System.in).nextLine();
+                        ClearConsole.clearConsole();
+                    } else {
+                        System.out.println("You run into a wall! You should try another way...");
+                    }
                     break;
 
                 case "look":
+                    System.out.println("you look around...");
+                    dungeon.printDungeon(player.getX(), player.getY());
+                    break;
 
                 case "inventory":
                     player.showInventory();
@@ -69,7 +116,27 @@ public class Game {
                     player.addItem(item);
                     break;
 
-                    case "removeitem":
+                    case "colors":
+
+                        System.out.println(Colors.blackColor+"test"+Colors.resetColor);
+                        System.out.println(Colors.redColor+"test"+Colors.resetColor);
+                        System.out.println(Colors.greenColor+"test"+Colors.resetColor);
+                        System.out.println(Colors.yellowColor+"test"+Colors.resetColor);
+                        System.out.println(Colors.blueColor+"test"+Colors.resetColor);
+                        System.out.println(Colors.magentaColor+"test"+Colors.resetColor);
+                        System.out.println(Colors.cyanColor+"test"+Colors.resetColor);
+                        System.out.println(Colors.whiteColor+"test"+Colors.resetColor);
+
+                        System.out.println(Colors.blackBackground+"test"+Colors.resetColor);
+                        System.out.println(Colors.redBackground+"test"+Colors.resetColor);
+                        System.out.println(Colors.greenBackground+"test"+Colors.resetColor);
+                        System.out.println(Colors.yellowBackground+"test"+Colors.resetColor);
+                        System.out.println(Colors.blueBackground+"test"+Colors.resetColor);
+                        System.out.println(Colors.magentaBackground+"test"+Colors.resetColor);
+                        System.out.println(Colors.cyanBackground+"test"+Colors.resetColor);
+                        System.out.println(Colors.whiteBackground+"test"+Colors.resetColor);
+                        System.out.println(""+Colors.resetColor);
+                        break;
 
                 case "takedamage":
                     String takeDamage = InputHandler.getInput("Enter damage amount: ");

f358a5954e426f0236b7359c6ac46482e0149487
Calle <ca.karlsson@hotmail.com>
2025-09-07 17:14:26 +0200

    Added more colors to use

--- src/main/java/org/game/utils/Colors.java
+++ src/main/java/org/game/utils/Colors.java
@@ -1,9 +1,23 @@
 package org.game.utils;
 
+
 public class Colors {
-    public static final String resetColor = "\u001B[0m";
-    public static final String redColor = "\u001B[31m";
-    public static final String yellowColor = "\u001B[33m";
-    public static final String greenColor = "\u001B[32m";
-    public static final String cyanColor = "\u001B[36m";
+    public static final String resetColor =         "\u001B[0m";
+    public static final String blackColor =         "\u001B[30m";
+    public static final String redColor =           "\u001B[31m";
+    public static final String greenColor =         "\u001B[32m";
+    public static final String yellowColor =        "\u001B[33m";
+    public static final String blueColor =          "\u001B[34m";
+    public static final String magentaColor =       "\u001B[35m";
+    public static final String cyanColor =          "\u001B[36m";
+    public static final String whiteColor =         "\u001B[37m";
+
+    public static final String blackBackground =    "\u001B[40m";
+    public static final String redBackground =      "\u001B[41m";
+    public static final String greenBackground =    "\u001B[42m";
+    public static final String yellowBackground =   "\u001B[43m";
+    public static final String blueBackground =     "\u001B[44m";
+    public static final String magentaBackground =  "\u001B[45m";
+    public static final String cyanBackground =     "\u001B[46m";
+    public static final String whiteBackground =    "\u001B[47m";
 }

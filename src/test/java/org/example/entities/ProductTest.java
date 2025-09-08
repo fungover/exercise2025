85f62ff0385973c10476df5c8a891a2747071694
Patrik <pat_p3@hotmail.com>
2025-09-08 16:49:00 +0200

    Refactor: use @BeforeEach to reduce test duplication but keeping one with all fields for Product

--- src/test/java/org/example/entities/ProductTest.java
+++ src/test/java/org/example/entities/ProductTest.java
@@ -1,13 +1,51 @@
 package org.example.entities;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProductTest {
 
+    private String id;
+    private String name;
+    private Category category;
+    private int rating;
+    private Product product;
+
+    @BeforeEach
+    void setUp() {
+        id = "1";
+        name = "Test product";
+        category = Category.ELECTRONICS;
+        rating = 8;
+        product = new Product(id, name, category, rating);
+    }
+
     @Test
     void shouldCreateProductWithId() {
+        assertEquals(id, product.id());
+    }
+
+    @Test
+    void shouldCreateProductWithIdAndName() {
+        assertEquals(id, product.id());
+        assertEquals(name, product.name());
+    }
+
+    @Test
+    void productShouldHaveCategory() {
+        assertEquals(category, product.category());
+    }
+
+    @Test
+    void productShouldHaveRating() {
+        assertEquals(rating, product.rating());
+    }
+
+    // Using this test for Given, When, Then reminder
+    @Test
+    void shouldHaveProductWithAllFields() {
         // Given
         String id = "1";
         String name = "Test product";
@@ -17,43 +55,8 @@ public class ProductTest {
         Product product = new Product(id, name, category, rating);
         // Then
         assertEquals(id, product.id());
-
-    }
-
-    @Test
-    void shouldCreateProductWithIdAndName() {
-        String id = "1";
-        String name = "Test product";
-        Category category = Category.ELECTRONICS;
-        int rating = 8;
-
-        Product product = new Product(id, name, category, rating);
-
-        assertEquals(id, product.id());
         assertEquals(name, product.name());
-    }
-
-    @Test
-    void productShouldHaveCategory() {
-        String id = "1";
-        String name = "Test product";
-        Category category = Category.ELECTRONICS;
-        int rating = 8;
-
-        Product product = new Product(id, name, category, rating);
-
         assertEquals(category, product.category());
-    }
-
-    @Test
-    void productShouldHaveRating() {
-        String id = "1";
-        String name = "Test product";
-        Category category = Category.ELECTRONICS;
-        int rating = 8;
-
-        Product product = new Product(id, name, category, rating);
-
         assertEquals(rating, product.rating());
     }
 }

1c581f2a03f9979a4989c4f341dcc37c0f6165fb
Emilyempa <emilypettersson@hotmail.com>
2025-09-08 12:27:45 +0200

    Add the first failing test

--- src/main/java/org/example/service/Warehouse.java
+++ src/main/java/org/example/service/Warehouse.java
@@ -1,4 +1,17 @@
 package org.example.service;
 
+import org.example.entities.Category;
+import org.example.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Warehouse {
+
+    private final List<Product> products = new ArrayList<>();
+
+    public List<Product> products() {
+        return products;
+    }
+
 }
--- src/test/java/org/example/service/WarehouseTest.java
+++ src/test/java/org/example/service/WarehouseTest.java
@@ -1,7 +1,45 @@
 package org.example.service;
 
+import org.example.entities.Category;
+import org.example.entities.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WarehouseTest {
 
+    private Warehouse warehouse;
+
+    @BeforeEach
+    void setUp() {
+        warehouse = new Warehouse();
+    }
+
+    @Test
+    @DisplayName( "Add a product to the warehouse")
+    void addProductToWarehouse(){
+        ZonedDateTime now = ZonedDateTime.now();
+        Product clicker = ( new Product(
+                "clicker-001",
+                "Clicker",
+                Category.TRAINING,
+                5,
+                now,
+                now
+        ));
+
+        warehouse.add(clicker);
+
+        List<Product> products = warehouse.products();
+
+        assertThat(products)
+                .hasSize(1)
+                .containsExactly(clicker);
+    }
 }

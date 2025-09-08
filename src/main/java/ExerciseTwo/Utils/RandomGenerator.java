d3c46d119efce582965b56826eb9d9f23057cc19
Louise <emmalouisekarlsson@hotmail.com>
2025-09-08 08:26:12 +0200

    Added check that value is equal och greater than + and illegal argument exception.

--- src/main/java/ExerciseTwo/Utils/RandomGenerator.java
+++ src/main/java/ExerciseTwo/Utils/RandomGenerator.java
@@ -5,6 +5,10 @@ import java.util.Random;
 public class RandomGenerator {
 
     public int getRandomNumber(int number){
+        if(number <= 0){
+            throw new IllegalArgumentException("Number must be greater than 0");
+        }
+
         Random rand = new Random();
         return rand.nextInt(number);
     }

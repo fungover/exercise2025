eeab0d5fc7787fc9892e20e575b40406ee75c515
Jan Elia <jan.elia995@hotmail.com>
2025-09-08 17:25:05 +0200

    add JUnit test plan

--- src/test/java/com/jan_elia/warehouse/service/WarehouseTest.java
+++ src/test/java/com/jan_elia/warehouse/service/WarehouseTest.java
@@ -1,4 +1,32 @@
 package com.jan_elia.warehouse.service;
 
-public class WarehouseTest {
-}
+// Test plan for Warehouse
+//
+// addProduct
+// TODO: success when adding a valid product
+// TODO: failure when product name is empty
+// TODO: failure when product id already exists
+//
+// updateProduct
+// TODO: success when updating an existing product
+// TODO: failure when product id is missing
+//
+// getAllProducts
+// TODO: success when all products are returned
+// TODO: failure when trying to modify the returned list
+//
+// getProductById
+// TODO: success when product id exists
+// TODO: failure when product id does not exist
+//
+// getProductsByCategorySorted
+// TODO: success when products are sorted A–Z
+// TODO: failure when category is null
+//
+// getProductsCreatedAfter
+// TODO: success when products after a date are returned
+// TODO: failure when date is null
+//
+// getModifiedProducts
+// TODO: success when only modified products are returned
+// TODO: success when no products are modified (empty list)

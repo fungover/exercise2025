0681793d79fbb6d5a552b650231ab62a061a84ce
Patrik <pat_p3@hotmail.com>
2025-09-08 16:05:27 +0200

    Add Product with id and name field (TDD)

--- src/main/java/org/example/entities/Product.java
+++ src/main/java/org/example/entities/Product.java
@@ -2,12 +2,18 @@ package org.example.entities;
 
 public class Product {
     String id;
+    String name;
 
-    public Product(String id) {
+    public Product(String id, String name) {
         this.id = id;
+        this.name = name;
     }
 
     public String id() {
         return id;
+    }
+
+    public String name() {
+        return name;
     }
 }
--- src/test/java/org/example/entities/ProductTest.java
+++ src/test/java/org/example/entities/ProductTest.java
@@ -10,10 +10,22 @@ public class ProductTest {
     void shouldCreateProductWithId() {
         // Given
         String id = "1";
+        String name = "Test product";
         // When
-        Product product = new Product(id);
+        Product product = new Product(id, name);
         // Then
         assertEquals(id, product.id());
 
+    }
+
+    @Test
+    void shouldCreateProductWithIdAndName() {
+        String id = "1";
+        String name = "Test product";
+
+        Product product = new Product(id, name);
+
+        assertEquals(id, product.id());
+        assertEquals(name, product.name());
     }
 }

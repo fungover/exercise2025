c9e29907e7f23c0ab4479bd61fcc0867351f7560
Patrik <pat_p3@hotmail.com>
2025-09-08 18:01:39 +0200

    Added empty addProduct method to Warehouse (TDD)

--- src/main/java/org/example/service/Warehouse.java
+++ src/main/java/org/example/service/Warehouse.java
@@ -1,5 +1,10 @@
 package org.example.service;
 
+import org.example.entities.Product;
+
 public class Warehouse {
 
+    public void addProduct(Product product) {
+
+    }
 }
--- src/test/java/org/example/service/WarehouseTest.java
+++ src/test/java/org/example/service/WarehouseTest.java
@@ -1,15 +1,36 @@
 package org.example.service;
 
+import org.example.entities.Category;
+import org.example.entities.Product;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class WarehouseTest {
 
+    private Warehouse warehouse;
+    private Product product;
+
+    @BeforeEach
+    void setUp() {
+        warehouse = new Warehouse();
+        product = new Product("1", "Test product", Category.ELECTRONICS, 8, LocalDate.now());
+    }
+
     @Test
     void shouldCreateEmptyWarehouse() {
         Warehouse warehouse = new Warehouse();
-
         assertNotNull(warehouse);
+    }
+
+    @Test
+    void shouldAddProduct() {
+        warehouse.addProduct(product);
+
+        assertDoesNotThrow(() -> warehouse.addProduct(product));
     }
 }

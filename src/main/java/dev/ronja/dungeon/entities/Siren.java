28c7e8be4eb6f66b13c42cc480306842702107d3
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2025-09-08 18:07:04 +0200

    (test) TDD for Siren class

--- src/main/java/dev/ronja/dungeon/entities/Siren.java
+++ src/main/java/dev/ronja/dungeon/entities/Siren.java
@@ -1,4 +1,13 @@
 package dev.ronja.dungeon.entities;
 
 public class Siren {
+    public void attack(Player player) {
+    }
+
+    public int getDamage() {
+        return 0;
+    }
+
+    public void sing(Player player) {
+    }
 }

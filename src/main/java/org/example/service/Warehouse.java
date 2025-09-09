a6c28a146d7e0886da5417e2bbf9db8e58979ad7
nilsishome <nilsnyberg24@gmail.com>
2025-09-09 17:59:58 +0200

    Add more tests + functionality in Warehouse class

--- src/main/java/org/example/service/Warehouse.java
+++ src/main/java/org/example/service/Warehouse.java
@@ -1,11 +1,14 @@
 package org.example.service;
 
+import org.example.entities.Category;
 import org.example.entities.Product;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Warehouse {
-    private final ArrayList<Product> products = new ArrayList<>();
+    private final List<Product> products = new ArrayList<>();
 
     public void addProduct(Product product) {
         if (product.name().isEmpty()) {
@@ -15,7 +18,23 @@ public class Warehouse {
         }
     }
 
-    public ArrayList<Product> getProducts() {
+    public void updateProduct(String id, String name, Category category, int rating) {}
+
+    public List<Product> getAllProducts() {
         return products;
+    }
+
+    public Product getProductById(String idNumber) {
+        return products.stream()
+                .filter(product -> product.id().equals(idNumber))
+                .toList()
+                .getFirst();
+    }
+
+    public List<Product> getProductsByCategorySorted(Category category) {
+        return products.stream()
+                .filter(product -> product.category().equals(category))
+                .sorted(Comparator.comparing(Product::name))
+                .toList();
     }
 }
--- src/test/java/org/example/service/WarehouseTest.java
+++ src/test/java/org/example/service/WarehouseTest.java
@@ -3,28 +3,106 @@ package org.example.service;
 import org.example.entities.Category;
 import org.example.entities.Product;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class WarehouseTest {
     private final Warehouse warehouse = new Warehouse();
 
     @Test
-    public void testAddProduct() {
+    public void testAddingProducts() {
         warehouse.addProduct(new Product("23", "Pants",
                 Category.CLOTHES, 5, LocalDateTime.now(), null));
 
-        List<Product> products = warehouse.getProducts();
-        assertEquals(1, products.size());
+        assertEquals(1, warehouse.getAllProducts().size());
     }
     @Test
-    public void testAddProductFail() {
+    public void testAddingProductsWithoutName() {
         // A product's name attribute must not be empty.
         warehouse.addProduct(new Product("34", "", Category.PROVISIONS,
                 0, LocalDateTime.now(), null));
 
-        assertEquals(0, warehouse.getProducts().size());
+        assertEquals(0, warehouse.getAllProducts().size());
+    }
+
+    @Test
+    public void testGettingAllProducts() {
+        warehouse.addProduct(new Product("23", "Pants",
+                Category.CLOTHES, 9, LocalDateTime.now(), null));
+        warehouse.addProduct(new Product("24", "Shirt",
+                Category.CLOTHES, 8, LocalDateTime.now(), null));
+        warehouse.addProduct(new Product("25", "Hat",
+                Category.CLOTHES, 3, LocalDateTime.now(), null));
+
+        assertEquals(3, warehouse.getAllProducts().size());
+    }
+
+    @Test
+    public void testGettingProductById() {
+        warehouse.addProduct(new Product("23", "Pants",
+                Category.CLOTHES, 9, LocalDateTime.now(), null));
+        warehouse.addProduct(new Product("24", "Shirt",
+                Category.CLOTHES, 8, LocalDateTime.now(), null));
+
+        Product shirt = warehouse.getAllProducts().getLast();
+
+        assertEquals(shirt, warehouse.getProductById("24"));
+    }
+    @Test
+    public void testCannotFindProductById() {
+        warehouse.addProduct(new Product("23", "Pants",
+                Category.CLOTHES, 9, LocalDateTime.now(), null));
+        warehouse.addProduct(new Product("24", "Shirt",
+                Category.CLOTHES, 8, LocalDateTime.now(), null));
+
+        assertThatThrownBy(() -> warehouse.getProductById("25"))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    public void testGettingProductsByCategorySorted() {
+        // Sorted by name A-Z.
+        Product pants = new Product("23", "Pants",
+                Category.CLOTHES, 9, LocalDateTime.now(), null);
+        Product shirt = new Product("24", "Shirt",
+                Category.CLOTHES, 8, LocalDateTime.now(), null);
+        Product hat = new Product("25", "Hat",
+                Category.CLOTHES, 3, LocalDateTime.now(), null);
+
+        warehouse.addProduct(pants); // index 0
+        warehouse.addProduct(shirt); // index 1
+        warehouse.addProduct(hat); // index 2
+
+        List<Product> sortedList = new ArrayList<>();
+        sortedList.add(0, hat);
+        sortedList.add(1, pants);
+        sortedList.add(2, shirt);
+
+        assertEquals(sortedList,
+                warehouse.getProductsByCategorySorted(Category.CLOTHES));
+    }
+    @Test
+    public void testCannotFindProductsByCategory() {
+        Product raisins = new Product("53", "Raisins",
+                Category.PROVISIONS, 7, LocalDateTime.now(), null);
+        Product banana = new Product("54", "Banana",
+                Category.PROVISIONS, 8, LocalDateTime.now(), null);
+
+        warehouse.addProduct(raisins); // index 0
+        warehouse.addProduct(banana); // index 1
+
+        List<Product> sortedList = new ArrayList<>();
+        sortedList.add(0, banana);
+        sortedList.add(1, raisins);
+
+        // We are trying to sort clothes, but only have provisions in our products.
+        assertNotEquals(sortedList,
+                warehouse.getProductsByCategorySorted(Category.CLOTHES));
     }
 }

1d232c2cc1225f76a6f93a0bf52e107ad5f27232
Patseroni <patrik.bustad@lerniastudent.se>
2025-09-10 13:41:05 +0200

    add random walls on the map

--- src/main/java/map/Dungeon.java
+++ src/main/java/map/Dungeon.java
@@ -6,17 +6,21 @@ public class Dungeon {
 
     private final int width, height;
     private final Tile[][] map;
+    private final int start = 0;
 
     public Dungeon(int width, int height) {
         this.width = width;
         this.height = height;
         map = new Tile[height][width];
 
+
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 map[y][x] = new Tile(TileType.FLOOR);
             }
         }
+
+
 
         // Walls around the map
         for (int x = 0; x < width; x++) {
@@ -27,6 +31,23 @@ public class Dungeon {
             map[y][0] = new Tile(TileType.WALL);
             map[y][width - 1] = new Tile(TileType.WALL);
         }
+
+        //Random placed walls
+        for(int x = 2; x < 7; x++){
+            map[3][x] = new Tile(TileType.WALL);
+        }
+        for(int x = 5; x < 9; x++){
+            map[6][x] = new Tile(TileType.WALL);
+        }
+        map[7][3] = new Tile(TileType.WALL);
+        map[7][2] = new Tile(TileType.WALL);
+        map[5][2] = new Tile(TileType.WALL);
+        map[5][1] = new Tile(TileType.WALL);
+        map[1][4] = new Tile(TileType.WALL);
+
+
+
+
 
 
 

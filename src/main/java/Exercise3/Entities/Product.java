e6345fba2f116c7dadbf36b36c20bcc959674707
Louise <emmalouisekarlsson@hotmail.com>
2025-09-11 14:55:13 +0200

    Added check that object isn't null and that created is after modified date.

--- src/main/java/Exercise3/Entities/Product.java
+++ src/main/java/Exercise3/Entities/Product.java
@@ -9,6 +9,9 @@ public record Product(String id, String name, Category category, int rating, Loc
     public Product{
        id = Objects.requireNonNull(id, "Id is null").trim();
        name = Objects.requireNonNull(name, "Name is null").trim();
+       Objects.requireNonNull(category, "Category is null");
+       Objects.requireNonNull(createdDate, "Created date is null");
+       Objects.requireNonNull(modifiedDate, "Modified Date is null");
 
        if(id.isEmpty() || name.isEmpty()){
            throw new IllegalArgumentException("Id or name is empty");
@@ -17,11 +20,15 @@ public record Product(String id, String name, Category category, int rating, Loc
         if(rating < 0 || rating > 10) {
             throw new IllegalArgumentException("Invalid rating");
         }
+
+        if(createdDate.isAfter(modifiedDate)){
+            throw new IllegalArgumentException("Modified date is before now");
+        }
     }
 
     public Product updateProduct(String id, String name, Category category, int rating) {
 
-        if (id.trim().isEmpty() || name.trim().isEmpty()) {
+        if(id.trim().isEmpty() || name.trim().isEmpty()) {
             throw new IllegalArgumentException("Id cannot be empty");
         }
 

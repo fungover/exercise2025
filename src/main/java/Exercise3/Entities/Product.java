42ca5fefdbd40af5e14b6b2fc2c0d3fdbb2a6c0d
Louise <emmalouisekarlsson@hotmail.com>
2025-09-11 13:32:34 +0200

    Added control that object not is null. .

--- src/main/java/Exercise3/Entities/Product.java
+++ src/main/java/Exercise3/Entities/Product.java
@@ -1,14 +1,18 @@
 package Exercise3.Entities;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public record Product(String id, String name, Category category, int rating, LocalDate createdDate,
                       LocalDate modifiedDate) {
 
     public Product{
-        if (id.trim().isEmpty() || name.trim().isEmpty()) {
-            throw new IllegalArgumentException("Id cannot be empty");
-        }
+       id = Objects.requireNonNull(id, "Id is null").trim();
+       name = Objects.requireNonNull(name, "Name is null").trim();
+
+       if(id.isEmpty() || name.isEmpty()){
+           throw new IllegalArgumentException("Id or name is empty");
+       }
 
         if(rating < 0 || rating > 10) {
             throw new IllegalArgumentException("Invalid rating");

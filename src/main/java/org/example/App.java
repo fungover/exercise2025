85a0fbe294f5b89c5aafd570035ca8a72a1228b1
Magnus <49763392+MagnusTerak@users.noreply.github.com>
2025-09-12 12:37:04 +0200

    Added methods for player Class & init playerName in app

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.example.entities.Inventory;
+import org.example.entities.Player;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        Player user = new Player("x", 10, 10, 10);
+        Inventory userInventory = new Inventory();
+
+        System.out.println("Welcome user! Please enter your name:");
+        Scanner scanner = new Scanner(System.in);
+        String playerName = scanner.nextLine();
+
+        user.setName(playerName);
+
+        System.out.println("Welcome " + playerName + ". Let your adventure begin!");
     }
 }
--- src/main/java/org/example/entities/Player.java
+++ src/main/java/org/example/entities/Player.java
@@ -1,6 +1,6 @@
 package org.example.entities;
 
-public abstract class Player {
+public class Player {
     private String name;
     private int damage;
     private int health;
@@ -15,5 +15,53 @@ public abstract class Player {
         this.maxHealth = maxHealth;
         this.positionX = 0;
         this.positionY = 0;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setMaxHealth(int maxHealth) {
+        this.maxHealth = maxHealth;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
     }
 }

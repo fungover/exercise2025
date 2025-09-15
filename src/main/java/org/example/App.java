1c667fed209ca03f6abfe1785b4a96a79dca0a71
Alfred Brännare <alfred-brannare@hotmail.com>
2025-09-15 18:01:18 +0200

    chore(App): wire ProductService with InMemoryProductRepository in main

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,11 @@
 package org.example;
 
-public class App {
+import org.example.repository.InMemoryProductRepository;
+import org.example.service.ProductService;
 
+public class App {
+    public static void main(String[] args) {
+        InMemoryProductRepository inMemoryProductRepository = new InMemoryProductRepository();
+        ProductService productService = new ProductService(inMemoryProductRepository);
+    }
 }

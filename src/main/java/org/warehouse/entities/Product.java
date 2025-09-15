f7f636ba90bb2e6b194245b3e16f549c274fdc7b
Kirill <kirillsavorin@gmail.com>
2025-09-15 16:45:58 +0200

    feat: product builder before private

--- src/main/java/org/warehouse/entities/Product.java
+++ src/main/java/org/warehouse/entities/Product.java
@@ -3,7 +3,11 @@ package org.warehouse.entities;
 import java.time.LocalDate;
 import java.util.Objects;
 
-public record Product(String id, String name, Category category, int rating, LocalDate createdDate,
+public record Product(String id,
+                      String name,
+                      Category category,
+                      int rating,
+                      LocalDate createdDate,
                       LocalDate modifiedDate) {
   public Product {
     Objects.requireNonNull(id, "id must not be null");
@@ -18,5 +22,48 @@ public record Product(String id, String name, Category category, int rating, Loc
     if (modifiedDate.isBefore(createdDate)) {
       throw new IllegalArgumentException("modifiedDate must not be before createdDate");
       }
+    }
+
+    public static class Builder {
+    private String id;
+    private String name;
+    private Category category;
+    private int rating;
+    private LocalDate createdDate;
+    private LocalDate modifiedDate;
+
+    public Builder setId(String id) {
+      this.id = id;
+      return this;
+    }
+
+    public Builder setName(String name) {
+      this.name = name;
+      return this;
+    }
+
+    public Builder setCategory(Category category) {
+      this.category = category;
+      return this;
+    }
+
+    public Builder setRating(int rating) {
+      this.rating = rating;
+      return this;
+    }
+
+    public Builder setCreatedDate(LocalDate createdDate) {
+      this.createdDate = createdDate;
+      return this;
+    }
+
+    public Builder setModifiedDate(LocalDate modifiedDate) {
+      this.modifiedDate = modifiedDate;
+      return this;
+    }
+
+    public Product build() {
+      return new Product(id, name, category, rating, createdDate, modifiedDate);
+    }
     }
 }

c8d42f8e06dcfefb0def57e9420f6b8675edd125
Rikard <rikardengstrom89@gmail.com>
2025-09-15 14:10:11 +0200

    Almost done with Product record.

--- src/main/java/org/example/entities/Product.java
+++ src/main/java/org/example/entities/Product.java
@@ -51,5 +51,61 @@ public record Product(
         private LocalDate createdDate;
         private LocalDate modifiedDate;
 
+        public Builder() {
+
+        }
+
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder category(Category category) {
+            this.category = category;
+            return this;
+        }
+
+        public Builder rating(int rating) {
+            this.rating = rating;
+            return this;
+        }
+
+        public Builder createdDate(LocalDate createdDate) {
+            this.createdDate = createdDate;
+            return this;
+        }
+
+        public Builder modifiedDate(LocalDate modifiedDate) {
+            this.modifiedDate = modifiedDate;
+            return this;
+        }
+
+        public Product build() {
+
+            if (id == null || id.trim().isEmpty()) {
+                throw new IllegalArgumentException("ID is required");
+            }
+            if (name == null || name.trim().isEmpty()) {
+                throw new IllegalArgumentException("Name is required");
+            }
+
+            if (category == null) {
+                throw new IllegalArgumentException("Category is required");
+            }
+
+            if (createdDate == null) {
+                this.createdDate = LocalDate.now();
+            }
+            if (modifiedDate == null) {
+                this.modifiedDate = LocalDate.now();
+            }
+            return new Product(this);
+        }
+
     }
 }

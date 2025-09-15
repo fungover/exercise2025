7feeed2fdb5550b913f919b01acd762b34b2afdb
Emilyempa <emilypettersson@hotmail.com>
2025-09-15 13:21:31 +0200

    Format changes

--- src/main/java/org/example/repository/ProductRepository.java
+++ src/main/java/org/example/repository/ProductRepository.java
@@ -1,12 +1,10 @@
 package org.example.repository;
 
-//Handles CRUD (Create, Read, Update, Delete).
-
 import org.example.entities.Product;
-
 import java.util.List;
 import java.util.Optional;
 
+//Handles CRUD (Create, Read, Update, Delete).
 public interface ProductRepository {
     void addProduct(Product product);
     Optional<Product> getProductById(String id);

a850d463e51f8feb07a4a056da758ecdb7af39a6
Calle <ca.karlsson@hotmail.com>
2025-09-16 11:59:02 +0200

    Create product test

---
+++ src/test/java/org/example/tdd/addProductTest.java
@@ -0,0 +1,11 @@
+package org.example.tdd;
+
+import org.junit.jupiter.api.Test;
+
+public class addProductTest {
+
+    @Test
+    void canCreateProduct() {
+    Product product = new Product();
+    }
+}

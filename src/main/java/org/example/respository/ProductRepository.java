b3085370074853ce56d2ddc9c6ddd98fdba88013
Rikard <rikardengstrom89@gmail.com>
2025-09-16 08:35:00 +0200

    Added ProductRepository interface in repository package. This marks the start och part 2 of the exercise.

---
+++ src/main/java/org/example/respository/ProductRepository.java
@@ -0,0 +1,16 @@
+package org.example.respository;
+
+import org.example.entities.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    void addProduct(Product product);
+
+    Optional<Product> getProductById(String id);
+
+    List<Product> getAllProducts();
+
+    void updateProduct(Product product);
+}

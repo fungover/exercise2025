2451d35d531e34ece0ba557e091076b44f3bd869
nilsishome <nilsnyberg24@gmail.com>
2025-09-16 20:02:54 +0200

    Fix potential bugs and nitpicks by CodeRabbit

--- src/test/java/org/example/service/MovementTest.java
+++ src/test/java/org/example/service/MovementTest.java
@@ -2,6 +2,7 @@ package org.example.service;
 
 import org.example.entities.Player;
 import org.example.map.Dungeon;
+import org.example.map.Tile;
 import org.example.utils.Enemies;
 import org.example.utils.ItemsOnFloor;
 import org.junit.jupiter.api.Test;
@@ -15,9 +16,18 @@ public class MovementTest {
     private final ItemsOnFloor itemsOnFloor = new ItemsOnFloor();
     private final Dungeon dungeon = new Dungeon(player, enemies, itemsOnFloor);
 
+    private void ensureClearCross(int x, int y) {
+        dungeon.setTile(y, x, Tile.FLOOR.getTile());
+        dungeon.setTile(y - 1, x, Tile.FLOOR.getTile());
+        dungeon.setTile(y + 1, x, Tile.FLOOR.getTile());
+        dungeon.setTile(y, x - 1, Tile.FLOOR.getTile());
+        dungeon.setTile(y, x + 1, Tile.FLOOR.getTile());
+    }
+
     @Test
     public void testMovingUp() {
         player.setPosition(3, 3);
+        ensureClearCross(3, 3);
         gameLogic.moveInput(dungeon, player, "U");
         assertThat(player.getX()).isEqualTo(3);
         assertThat(player.getY()).isEqualTo(2);
@@ -26,6 +36,7 @@ public class MovementTest {
     @Test
     public void testMovingDown() {
         player.setPosition(3, 3);
+        ensureClearCross(3, 3);
         gameLogic.moveInput(dungeon, player, "D");
         assertThat(player.getX()).isEqualTo(3);
         assertThat(player.getY()).isEqualTo(4);
@@ -34,6 +45,7 @@ public class MovementTest {
     @Test
     public void testMovingLeft() {
         player.setPosition(3, 3);
+        ensureClearCross(3, 3);
         gameLogic.moveInput(dungeon, player, "L");
         assertThat(player.getX()).isEqualTo(2);
         assertThat(player.getY()).isEqualTo(3);
@@ -42,6 +54,7 @@ public class MovementTest {
     @Test
     public void testMovingRight() {
         player.setPosition(3, 3);
+        ensureClearCross(3, 3);
         gameLogic.moveInput(dungeon, player, "R");
         assertThat(player.getX()).isEqualTo(4);
         assertThat(player.getY()).isEqualTo(3);

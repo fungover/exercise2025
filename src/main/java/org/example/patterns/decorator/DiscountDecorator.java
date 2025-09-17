cb29466c84fd2ba7ad9c1518b2c86494de74e7bd
Emilyempa <emilypettersson@hotmail.com>
2025-09-17 15:22:12 +0200

    Add DiscountDecorator to validate discount percentage and apply precise rounding to calculated prices

--- src/main/java/org/example/patterns/decorator/DiscountDecorator.java
+++ src/main/java/org/example/patterns/decorator/DiscountDecorator.java
@@ -1,4 +1,29 @@
 package org.example.patterns.decorator;
 
-public class DiscountDecorator{
+import org.example.entities.Sellable;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class DiscountDecorator extends ProductDecorator {
+    private final BigDecimal discountPercent;
+
+    public DiscountDecorator(Sellable decoratedProduct, double discountPercentage) {
+        super(decoratedProduct);
+        if (discountPercentage < 0 || discountPercentage > 100) {
+            throw new IllegalArgumentException("Discount percentage must be between 0 and 100: " +
+                    discountPercentage);
+        }
+
+        this.discountPercent = BigDecimal.valueOf(discountPercentage);
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        BigDecimal discountFactor = BigDecimal.ONE
+                .subtract(discountPercent.divide(BigDecimal.valueOf(100), RoundingMode.HALF_UP));
+        return decoratedProduct.getPrice()
+                .multiply(discountFactor)
+                .setScale(2, RoundingMode.HALF_UP);
+    }
 }
+

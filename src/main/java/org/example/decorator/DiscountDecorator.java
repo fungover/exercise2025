f57aa36949db8f4a53b23dfca1b683c943b8668d
Rikard <rikardengstrom89@gmail.com>
2025-09-18 09:42:54 +0200

    Commenting some code.

--- src/main/java/org/example/decorator/DiscountDecorator.java
+++ src/main/java/org/example/decorator/DiscountDecorator.java
@@ -2,6 +2,15 @@ package org.example.decorator;
 
 public class DiscountDecorator extends ProductDecorator {
 
+    /**
+     * DiscoundDecorator - Adds discount functionality to any Sellable.
+     * <p>
+     * HOW IT WORKS:
+     * - Takes original Sellable + discount percentage.
+     * - Keeps ID and name unchanged (inherits from parent).
+     * - Overrides getPrice() to apply discount.
+     */
+
     private double discountPercentage;
 
     public DiscountDecorator(Sellable decoratedProduct, double discountPercentage) {

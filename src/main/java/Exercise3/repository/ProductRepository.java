bfd7432f74f46cb35fe77e6c924060840a7ad25f
Louise <emmalouisekarlsson@hotmail.com>
2025-09-18 16:02:42 +0200

    Created an interface for ProductRepository and a class that implements ProductRepository and stores products in a list.

--- src/main/java/Exercise3/repository/ProductRepository.java
+++ src/main/java/Exercise3/repository/ProductRepository.java
@@ -1,4 +1,18 @@
 package exercise3.repository;
 
+import exercise3.entities.Product;
+
+import java.util.List;
+
 public interface ProductRepository {
+
+    void addProduct(Product product);
+
+    void updateProduct(Product product);
+
+    List<Product> getAllProducts();
+
+    Product getProductById(String id);
+
+
 }

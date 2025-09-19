50af0d7307d9ce0c270ff90926417a6002b33c4c
Deansie <dennis-andersen@outlook.com>
2025-09-19 09:43:20 +0200

    chore(repository): add ProductRepository interface

--- src/main/java/org/example/repository/ProductRepository.java
+++ src/main/java/org/example/repository/ProductRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface ProductRepository {
     void addProduct(Product product);
-    Optional<Product> getProductById(String id);
-    List<Product> getAllProducts();
     void updateProduct(Product product);
+    List<Product> getAllProducts();
+    Optional<Product> getProductById(String id);
 }

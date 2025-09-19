1dfd7b3c0cabea231f145549027271843172d780
Tobias Larsson <tobbe-larsson@hotmail.com>
2025-09-19 12:36:25 +0200

    Integrate `ProductService` and `ProductRepository` into `App`'s main method.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.example.repository.InMemoryProductRepository;
+import org.example.repository.ProductRepository;
+import org.example.service.ProductService;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        // Create repository
+        ProductRepository repo = new InMemoryProductRepository();
+        //Create service and inject repository
+        ProductService productService = new ProductService(repo);
+
+        System.out.println("Antal produkter: " + productService.getAllProducts().size());
     }
 }

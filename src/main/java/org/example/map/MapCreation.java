e553c1c6d158fc160dae837c8f0f8c6c807566c6
Magnus <49763392+MagnusTerak@users.noreply.github.com>
2025-09-22 19:08:38 +0200

    Made the map  random generated for Enemies and chests

--- src/main/java/org/example/map/MapCreation.java
+++ src/main/java/org/example/map/MapCreation.java
@@ -2,20 +2,28 @@ package org.example.map;
 
 import org.example.entities.Enemy;
 import org.example.entities.Item;
+import java.util.Random;
 
 public class MapCreation {
     private final Tile[][] grid;
 
     public MapCreation(int width, int height) {
+        Random rand = new Random();
         grid = new Tile[width][height];
         for(int x = 0; x < width; x++) {
             for(int y = 0; y < height; y++) {
-                grid[x][y] = new Tile(TileEnum.EMPTY, null, null);
+                double roll = rand.nextDouble();
+
+                if (roll < 0.20) {
+                    grid[x][y] = new Tile(TileEnum.CHEST, null, new Item("Health potion", "health_potion"));
+                } else if (roll < 0.40) {
+                    grid[x][y] = new Tile(TileEnum.ENEMY, new Enemy(getRandomEnemy(), 20, 5, x, y), null);
+                } else {
+                    grid[x][y] = new Tile(TileEnum.EMPTY, null, null);
+                }
+
             }
         }
-
-        grid[1][1] = new Tile(TileEnum.ENEMY, new Enemy("Skeleton", 20, 4, 1, 1), null);
-        grid[0][1] = new Tile(TileEnum.CHEST, null, new Item("Health potion", "health_potion"));
     }
 
     public Tile getTile(int x, int y) {
@@ -30,5 +38,12 @@ public class MapCreation {
         }
 
         return true;
+    }
+
+    private String getRandomEnemy() {
+        String[] Enemies = {"Skeleton", "Knight", "Orc"};
+
+        int index = new Random().nextInt(Enemies.length);
+        return Enemies[index];
     }
 }

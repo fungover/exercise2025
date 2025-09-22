ecc331d03ce84f3e28d9e97a69326a9e838e03e8
jennymakki <jjennyeklundd@hotmail.com>
2025-09-22 16:55:45 +0200

    Changed from EmailMessageService to SMSMessageService to make sure it works to just change the Dependency in App.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,12 @@
 package org.example;
 
 import org.example.printer.MessagePrinter;
-import org.example.service.EmailMessageService;
 import org.example.service.MessageService;
+import org.example.service.SMSMessageService;
 
 public class App {
     public static void main(String[] args) {
-        MessageService service = new EmailMessageService();
+        MessageService service = new SMSMessageService();
         MessagePrinter printer = new MessagePrinter(service);
 
         printer.print();

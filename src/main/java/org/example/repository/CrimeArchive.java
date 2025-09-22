2dd09bc14934ec88d1a44bb96eb334db3b2f1809
Emilyempa <emilypettersson@hotmail.com>
2025-09-22 11:58:26 +0200

    Implement Crime interface in CrimeArchive with getCrimeDetails method

--- src/main/java/org/example/repository/CrimeArchive.java
+++ src/main/java/org/example/repository/CrimeArchive.java
@@ -1,4 +1,8 @@
 package org.example.repository;
 
-public class CrimeArchive {
+public class CrimeArchive implements Crime {
+    @Override
+    public String getCrimeDetails(String caseNumber){
+        return "Crime scene: Baker street. Witness: Mrs Hudson.";
+    }
 }

b10f832e41d41ab0468a1d3a49628008bdcb2f0c
jennymakki <jjennyeklundd@hotmail.com>
2025-09-22 17:25:52 +0200

    Created SimpleContainer that will now pass the dependency.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,18 @@
 package org.example;
 
+import org.example.container.SimpleContainer;
 import org.example.printer.MessagePrinter;
+import org.example.service.EmailMessageService;
 import org.example.service.MessageService;
 import org.example.service.SMSMessageService;
 
 public class App {
     public static void main(String[] args) {
-        MessageService service = new SMSMessageService();
-        MessagePrinter printer = new MessagePrinter(service);
+        SimpleContainer container = new SimpleContainer();
+
+        container.register(MessageService.class, SMSMessageService.class);
+
+        MessagePrinter printer = container.getInstance(MessagePrinter.class);
 
         printer.print();
     }

092956ba2ce3085ab996a9bbbf7fe42517bd2f52
Emilyempa <emilypettersson@hotmail.com>
2025-09-22 11:58:43 +0200

    Implement Detective interface in SherlockHolmes class with investigate method

--- src/main/java/org/example/detective/SherlockHolmes.java
+++ src/main/java/org/example/detective/SherlockHolmes.java
@@ -1,5 +1,17 @@
 package org.example.detective;
 
-public class SherlockHolmes {
+import org.example.repository.Crime;
+
+public class SherlockHolmes implements Detective {
     private final Crime crime;
+
+    public SherlockHolmes(Crime crime) {
+        this.crime = crime;
+    }
+
+    @Override
+    public String investigate(String caseNumber) {
+        String clues= crime.getCrimeDetails(caseNumber);
+        return "Investigating case number: " + caseNumber + " with clues: " + clues;
+    }
 }

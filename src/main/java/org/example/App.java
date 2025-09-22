ff49ed7fb460a703c479c471b229c5388db2b83a
Emilyempa <emilypettersson@hotmail.com>
2025-09-22 12:52:37 +0200

    Integrate Investigation class with Detective and update App to initiate investigation process

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.example.detective.SherlockHolmes;
+import org.example.repository.CrimeArchive;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        var crimeRepo = new CrimeArchive();
+        var detective = new SherlockHolmes(crimeRepo);
+        var engine = new Investigation(detective);
+
+        engine.startInvestigation("CASE-221B");
     }
 }
--- src/main/java/org/example/Investigation.java
+++ src/main/java/org/example/Investigation.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import org.example.detective.Detective;
+
 public class Investigation {
+    private final Detective detective;
+
+    public Investigation(Detective detective) {
+        this.detective = detective;
+    }
+
+    public void startInvestigation(String caseNumber) {
+        System.out.println(detective.investigate(caseNumber));
+    }
 }

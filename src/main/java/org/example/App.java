7f03fa27d869535a1655b176a5cfe6a534fb21e7
Tobias Larsson <tobbe-larsson@hotmail.com>
2025-09-23 10:18:04 +0200

    Part 3: Refactor App.java main to use Weld for CDI initialization and service injection

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,14 +4,16 @@ import org.example.persistence.DataRepository;
 import org.example.persistence.DatabaseRepository;
 import org.example.service.DataService;
 import org.example.service.SimpleDataService;
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
 
 public class App {
     public static void main(String[] args) {
-        // Create dependency
-        DataRepository repository = new DatabaseRepository();
-        // Send dependency to constructor
-        DataService service = new SimpleDataService(repository);
-        // Use service
-        service.process("Hello Dependency Injection!");
+        Weld weld = new Weld();
+        try (WeldContainer container = weld.initialize()) {
+            DataService service = container.select(SimpleDataService.class).get();
+
+        service.process("Hello CDI Part 3 !");
+        }
     }
 }

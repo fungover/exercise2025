5b8219ca071c8abba57ee4404a19f995dcf51334
Patrik <pat_p3@hotmail.com>
2025-09-23 15:12:02 +0200

    Updated ComputerBuilder with AssemblyService injection

--- src/main/java/org/example/part1/ComputerBuilder.java
+++ src/main/java/org/example/part1/ComputerBuilder.java
@@ -1,21 +1,24 @@
 package org.example.part1;
 
+import org.example.part1.services.AssemblyService;
 import org.example.part1.services.CPUService;
 import org.example.part1.services.GPUService;
 
 public class ComputerBuilder {
     private final CPUService cpuService;
     private final GPUService gpuService;
+    private final AssemblyService assemblyService;
 
-    public ComputerBuilder(CPUService cpuService, GPUService gpuService) {
+    public ComputerBuilder(CPUService cpuService, GPUService gpuService, AssemblyService assemblyService) {
         this.cpuService = cpuService;
         this.gpuService = gpuService;
+        this.assemblyService = assemblyService;
     }
 
     public String buildComputer(String purpose) {
         String cpu = cpuService.selectCPU(purpose);
         String gpu = gpuService.selectGPU(purpose);
 
-        return "Computer built with " + cpu + " and " + gpu;
+        return assemblyService.assembleComputer(cpu, gpu, purpose);
     }
 }

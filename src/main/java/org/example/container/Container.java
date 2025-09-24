8a6427cc0029b9c04e5cfcc7863537d562831b40
marcusmoller97 <marcusssmoller@gmail.com>
2025-09-24 15:48:57 +0200

    fixed coderabbit suggestions

--- src/main/java/org/example/container/Container.java
+++ src/main/java/org/example/container/Container.java
@@ -7,15 +7,16 @@ import java.util.Map;
 public class Container {
     private final Map<Class<?>, Class<?>> bindings = new HashMap<>();
 
-    public Map<Class<?>, Class<?>> getBindings() {
-        return bindings;
-    }
-
     public <T> T create(Class<?> clazz) throws Exception {
         Class<?> impl = bindings.getOrDefault(clazz, clazz);
 
         // get constructor
-        Constructor<?> constructor = impl.getDeclaredConstructors()[0];
+        Constructor<?>[] constructors = impl.getDeclaredConstructors();
+        if (constructors.length == 0) {
+            throw new IllegalArgumentException("No constructors found for class: " + impl.getName());
+        }
+        // For simplicity, use the first constructor, but document this limitation
+        Constructor<?> constructor = constructors[0];
         constructor.setAccessible(true);
 
         // get parameters from constructor
@@ -37,17 +38,3 @@ public class Container {
     }
 
 }
-
-// TODO: Handle contructor injection
-// TODO: Här är en lista på punkter som behöver täckas för Part 2 – A Minimal DI Container:
-// TODO: Skapa en container-klass som kan hantera objektinstansiering. Done
-//TODO: En metod som returnerar en instans av en given klass (Class<T>). Done
-//TODO: Hämta konstruktorn för den begärda klassen. Done
-//TODO: Identifiera om konstruktorn har parametrar (beroenden). Done
-//TODO: Skapa parametrarna rekursivt genom att anropa containerns metod igen.
-//TODO: Bygga instansen av klassen med dess beroenden via konstruktorn.
-//TODO: Returnera den färdiga instansen till anroparen.
-//TODO: Hantera endast en konstruktor per klass (som föreskrivs i uppgiften).
-//TODO: Automatiskt bygga hela dependency graphen när man ber om en top-level klass.
-//TODO: (Valfritt men bra)
-//TODO: Möjlighet att mappa interfaces till konkreta implementationer, så att containern vet vilken klass som ska skapas när ett interface efterfrågas.

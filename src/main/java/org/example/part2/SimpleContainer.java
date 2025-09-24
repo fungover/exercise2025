51858fe4e635a425f3fbb879395ce0683cf56e22
Patrik <pat_p3@hotmail.com>
2025-09-24 14:18:19 +0200

    Added constructor parameter injection with recursive creation

--- src/main/java/org/example/part2/SimpleContainer.java
+++ src/main/java/org/example/part2/SimpleContainer.java
@@ -1,5 +1,6 @@
 package org.example.part2;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class SimpleContainer {
@@ -8,9 +9,39 @@ public class SimpleContainer {
         try {
             System.out.println("Creating instance of: " + clazz.getName());
 
-            return clazz.getDeclaredConstructor().newInstance();
+            // Get all constructors
+            Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+
+            if (constructors.length == 0) {
+                throw new RuntimeException("No constructors found for " + clazz.getName());
+            }
+
+            // Take the first constructor assuming only one constructor per class
+            Constructor<?> constructor = constructors[0];
+            Class<?>[] parameterTypes = constructor.getParameterTypes();
+
+            System.out.println("Constructor has " + parameterTypes.length + " parameters");
+
+            if (parameterTypes.length == 0) {
+                // Default constructor
+                return (T) constructor.newInstance();
+            } else {
+                // Constructor with parameters - recursion
+                Object[] dependencies = new Object[parameterTypes.length];
+
+                for (int i = 0; i < parameterTypes.length; i++) {
+                    System.out.println(" Creating dependency " + (i + 1) + " : " +
+                            parameterTypes[i].getSimpleName());
+
+                    dependencies[i] = getInstance(parameterTypes[i]); // Recursive call
+                }
+
+                return (T) constructor.newInstance(dependencies);
+            }
+
         } catch (Exception e) {
-            System.out.println("Failed to create " + clazz.getName() + ": " + e.getMessage());
+            System.out.println("Failed to create " + clazz.getSimpleName() + ": " + e.getMessage());
+            e.printStackTrace();
             return null;
         }
 

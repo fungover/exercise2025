3bf306e098a004f55c967054d7213dae18a8df2c
Johnny Astrom <johnny.astrom@hotmail.com>
2025-09-24 15:54:28 +0200

    Part 2: Make first test pass with hardcoded DIContainer

--- src/main/java/org/example/di/part2_minimal_di_container/DIContainer.java
+++ src/main/java/org/example/di/part2_minimal_di_container/DIContainer.java
@@ -1,7 +1,13 @@
 package org.example.di.part2_minimal_di_container;
 
+import org.example.di.part1_manual_constructor_injection.*;
+
 public class DIContainer {
     public <T> T getInstance(Class<T> clazz) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        // Hardcode: always returns customer with swish payment
+        if (clazz == Customer.class) {
+            return (T) new Customer(new SimpleCheckoutService(new SwishPayment()));
+        }
+        throw new IllegalArgumentException("Unsupported class: " + clazz);
     }
 }

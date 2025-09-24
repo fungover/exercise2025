d11da2781b182611966c80eeb797488766d1c82a
Louise <emmalouisekarlsson@hotmail.com>
2025-09-24 12:26:36 +0200

    Added explaining to params in record

--- src/main/java/exercise5/entities/Book.java
+++ src/main/java/exercise5/entities/Book.java
@@ -2,6 +2,14 @@ package exercise5.entities;
 
 public record Book(String titel, String author, Genre genre, int rating){
 
+    /**
+     * *
+     * @param titel specifies the titel of the book
+     * @param author specifies the author of the book
+     * @param genre enum, specifies the genre of the book
+     * @param rating 0-10, specifies the rating of the book
+     */
+
     public Book {
         if(rating < 0 || rating > 10) {
             throw new IllegalArgumentException("Invalid rating");

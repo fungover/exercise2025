86a0e7ec0abe4e2756b76d8907d5ff2d7cd20558
Patrik <pat_p3@hotmail.com>
2025-09-24 18:07:25 +0200

    Added Part3Main to showcase Weld CDI

---
+++ src/main/java/org/example/part3/Part3Main.java
@@ -0,0 +1,23 @@
+package org.example.part3;
+
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+
+public class Part3Main {
+    static void main(String[] args) {
+        System.out.println("\nWeld CDI\n");
+
+        Weld weld = new Weld();
+        WeldContainer container = weld.initialize();
+
+        ComputerBuilder builder = container.select(ComputerBuilder.class).get();
+
+        String gamingPC = builder.buildComputer("Gaming");
+        System.out.println(gamingPC);
+
+        weld.shutdown();
+        System.out.println("Weld CDI completed successfully!");
+    }
+
+}
+

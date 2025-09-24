ef2597c90a85b356398faa9b8678d3c479dd9b2b
Alfred Brännare <alfred-brannare@hotmail.com>
2025-09-24 13:01:25 +0200

    fix: add validation and caching to DI container

--- src/main/java/org/example/di/SimpleDIContainer.java
+++ src/main/java/org/example/di/SimpleDIContainer.java
@@ -18,22 +18,38 @@ public class SimpleDIContainer implements DIContainer {
 
     @Override
     public <T> T resolve(Class<T> type) {
-        Class<?> implClass = registrations.get(type);
-        Constructor<?>[] constructors = implClass.getConstructors();
-        Constructor<?> constructor = constructors[0];
-        Class<?>[] parameterTypes = constructor.getParameterTypes();
-        Object[] parameter = new Object[parameterTypes.length];
+        if (instances.containsKey(type)) {
+            return (T) instances.get(type);
+        }
 
-        for (int i = 0; i < parameterTypes.length; i++) {
-            parameter[i] = resolve(parameterTypes[i]);
+        Class<?> implClass = registrations.get(type);
+        if (implClass == null) {
+            throw new RuntimeException("No implementation found for " + type.getName());
         }
 
         try {
+            Constructor<?>[] constructors = implClass.getConstructors();
+            if (constructors.length == 0) {
+                throw new RuntimeException("No constructors found for: " + implClass.getName());
+            }
+
+            Constructor<?> constructor = constructors[0];
+            Class<?>[] parameterTypes = constructor.getParameterTypes();
+            Object[] parameter = new Object[parameterTypes.length];
+
+            for (int i = 0; i < parameterTypes.length; i++) {
+                parameter[i] = resolve(parameterTypes[i]);
+            }
+
             T instance = (T) constructor.newInstance(parameter);
+
+            instances.put(type, instance);
+
             return instance;
         } catch (Exception e) {
             throw new RuntimeException("Failed to create instance", e);
         }
+
 
     }
 }

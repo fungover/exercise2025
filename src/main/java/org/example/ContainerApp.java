5bb32ce994a5e1d5c04d40f68bf53122f185eea2
Rikard <rikardengstrom89@gmail.com>
2025-09-25 13:02:42 +0200

    Cleaned upp containerapp

--- src/main/java/org/example/ContainerApp.java
+++ src/main/java/org/example/ContainerApp.java
@@ -14,38 +14,24 @@ public class ContainerApp {
 
         System.out.println("=== Testing Simple container with Reflection ===");
 
-        System.out.println("Test 1: EmailService + DatabaseRepository");
+// === Test 1: EmailService + DatabaseRepository ===
+        SimpleContainer container = new SimpleContainer();
 
+// Configures the container - tells which implementation to use.
+// "When someone asks for DataRepository, give them DatabaseRepository".
+        container.bind(DataRepository.class, DatabaseRepository.class);
+// "When someone asks for MessageService, give them EmailMessageService".
+        container.bind(MessageService.class, EmailMessageService.class);
 
-        SimpleContainer container1 = new SimpleContainer(); // Creates a mew container class.
-        SimpleContainer container2 = new SimpleContainer();
-
-        // Configures the container - tells which implementation to use.
-        // "When someone asks for DataRepository, give them DatabaseRepository".
-        container1.bind(DataRepository.class, DatabaseRepository.class);
-        // "When someone asks for MessageService, give them EmailMessageService".
-        container1.bind(MessageService.class, EmailMessageService.class);
-
-        container2.bind(DataRepository.class, FileRepository.class);
-        container2.bind(MessageService.class, SmsMessageService.class);
-
-        /**
-         * Here, we only ask for MessageService - nothing else.
-         * The container will automatically:
-         * 1. See that MessageService -> EmailMessageService.
-         * 2. See that EmailMessageService needs DataRepository in the constructor.
-         * 3. See that DataRepository -> DatabaseRepository.
-         * 4. Create a DatabaseRepository first (no dependencies).
-         * 5. Use DatabaseRepository to create EmailMessageService.
-         * 6. Return the fully created EmailMessageService as MessageService.
-         */
-
-        System.out.println("=== ");
-
-        MessageService service1 = container1.getInstance(MessageService.class);
+        MessageService service1 = container.getInstance(MessageService.class);
         service1.processMessage();
 
-        MessageService service2 = container2.getInstance(MessageService.class);
+// === Test 2: SmsService + FileRepository ===
+// Rebind dependencies to simulate a new configuration
+        container.bind(DataRepository.class, FileRepository.class);
+        container.bind(MessageService.class, SmsMessageService.class);
+
+        MessageService service2 = container.getInstance(MessageService.class);
         service2.processMessage();
 
 

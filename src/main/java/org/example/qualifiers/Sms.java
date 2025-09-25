127f7fb57bac278a44dea373296f9c4ddec63892
Rikard <rikardengstrom89@gmail.com>
2025-09-25 12:59:38 +0200

    Added few comments.

--- src/main/java/org/example/qualifiers/Sms.java
+++ src/main/java/org/example/qualifiers/Sms.java
@@ -10,12 +10,12 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Qualifier
-@Retention(RUNTIME)
-@Target({TYPE, METHOD, FIELD, PARAMETER})
+@Qualifier // Marks this annotation as a CDI qualifier
+@Retention(RUNTIME) // Specifies that this annotation will be available at runtime
+@Target({TYPE, METHOD, FIELD, PARAMETER}) // Specifies where this annotation can be applied
 public @interface Sms {
     class Literal extends AnnotationLiteral<Sms> implements Sms {
         @Serial
-        private static final long serialVersionUID = 1L;
+        private static final long serialVersionUID = 1L; // Unique identifier for serialization
     }
 }

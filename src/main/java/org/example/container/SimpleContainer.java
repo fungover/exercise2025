a0af818e3513e7ba106de253cd6632de1592527b
Rikard <rikardengstrom89@gmail.com>
2025-09-26 14:05:29 +0200

    Previous reviewer wanted me to check for the constructor with most parameters. This has been reverted from feedback on todays lecture. Instead we use the @Inject annotation to find the right constructor in our DI-container.

--- src/main/java/org/example/container/SimpleContainer.java
+++ src/main/java/org/example/container/SimpleContainer.java
@@ -1,76 +1,102 @@
 package org.example.container;
 
+import jakarta.inject.Inject;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Parameter;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SimpleContainer {
 
-    private final Map<Class<?>, Class<?>> bindings = new HashMap<>(); // Holds interface and the concrete class (example: DataRepository -> DatabaseRepository)
+    private final Map<Class<?>, Class<?>> bindings = new HashMap<>(); // Map to store bidings between interface and implementation (E.g., MessageService -> EmailMessageService)
 
-    // Holds its own stack (per thread) over what types are currently about to be instantiated.
-    // This is to detect circular dependencies.
-    private final ThreadLocal<Deque<Class<?>>> resolving =
+
+    private final ThreadLocal<Deque<Class<?>>> resolving = //ThreadLocal stack to track all types that are currently being resolved to detect circular dependencies.
             ThreadLocal.withInitial(ArrayDeque::new);
 
-    public <T> void bind(Class<T> interfaceClass, Class<? extends T> implementationClass) {
-        bindings.put(interfaceClass, implementationClass); // Saves what implementation to use for the given interface.
+    public <T> void bind(Class<T> interfaceClass, Class<? extends T> implementationClass) { // Method to bind an interface to its implementation
+        bindings.put(interfaceClass, implementationClass);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getInstance(Class<T> requestedType) {
-        Deque<Class<?>> stack = resolving.get(); // Gets the stack for this thread.
+        Deque<Class<?>> stack = resolving.get(); // Get the current stack of types being resolved
 
-
-        if (stack.contains(requestedType)) { // If the stack already contains the type that we are about to create, we have a circular dependency.
-            throw new IllegalStateException("Circular dependency detected: " + stack + " -> " + requestedType); // Example: A -> B -> C -> A and we are about to create A again. Then we throw an exception.
+        if (stack.contains(requestedType)) {
+            throw new IllegalStateException("Circular dependency detected: " + stack + " -> " + requestedType); // Check for circular dependency
         }
 
-        stack.push(requestedType); // If no circular dependency, we push the type that we are about to create.
+        stack.push(requestedType); // Push the requested type onto the stack
         try {
-            Class<?> implementationClass = requestedType; // Default to the requested type.
+            Class<?> implementationClass = requestedType; // Default to the requested type
 
-            if (requestedType.isInterface()) { // If the requested type is an interface, we need to find the implementation.
-                Class<?> implementation = bindings.get(requestedType); // Look up the implementation in the bindings map.
+            //If the requested type is an interface, look up its implementation in the bindings map.
+            if (requestedType.isInterface()) {
+                Class<?> implementation = bindings.get(requestedType);
                 if (implementation == null) {
-                    throw new ContainerException("No implementation found for " + requestedType); // If no implementation is found, we throw an exception.
+                    throw new ContainerException("No implementation found for " + requestedType);
                 }
-                implementationClass = implementation; // Otherwise, we set the implementation class to the found implementation.
+                implementationClass = implementation;
             }
 
-            Constructor<?>[] constructors = implementationClass.getConstructors(); // Get all public constructors.
-            if (constructors.length == 0) {
-                throw new ContainerException("No public constructors found for " + implementationClass.getName()); // If no public constructors, we throw an exception.
-            }
-            Constructor<?> constructor = constructors[0]; // Start by assuming the first constructor is the one we want.
-            for (Constructor<?> c : constructors) { // Iterate through all constructors to find the one with the most parameters.
-                if (c.getParameterCount() > constructor.getParameterCount()) { // We choose the constructor with the most parameters (the "greediest" one).
-                    constructor = c; // Update the constructor to the one with more parameters.
-                }
-            }
+            Constructor<?> constructor = chooseConstructor(implementationClass); // Choose the appropriate constructor with our helper method.
 
-            if (constructor.getParameterCount() == 0) { // If the constructor has no parameters, we can create the instance directly.
+            // If the constructor has no parameters, simply instantiate it.
+            if (constructor.getParameterCount() == 0) {
                 return (T) constructor.newInstance();
             }
 
-            Parameter[] parameters = constructor.getParameters(); // Get the parameters of the constructor.
-            Object[] dependencies = new Object[parameters.length]; // Array to hold the dependencies.
+            // For constructors with parameters, resolve each dependency recursively.
+            Parameter[] parameters = constructor.getParameters();
+            Object[] dependencies = new Object[parameters.length];
             for (int i = 0; i < parameters.length; i++) {
-                dependencies[i] = getInstance(parameters[i].getType()); // Recursively get the instance for each parameter type.
+                dependencies[i] = getInstance(parameters[i].getType());
             }
 
-            return (T) constructor.newInstance(dependencies); // Create the instance with the resolved dependencies.
+            return (T) constructor.newInstance(dependencies); // Instantiate the object with its dependencies.
         } catch (Exception e) {
             throw new ContainerException("Could not create instance of " + requestedType.getName(), e);
         } finally {
-            stack.pop(); // Remove the type from the stack when done.
+            stack.pop(); // Pop the type off the stack after resolution is complete
             if (stack.isEmpty()) {
-                resolving.remove(); // Clean up the ThreadLocal if the stack is empty.
+                resolving.remove(); // Clean up the ThreadLocal if the stack is empty
             }
         }
     }
-}
 
+    private Constructor<?> chooseConstructor(Class<?> impl) { // Helper method to choose the appropriate constructor for instantiation.
+        Constructor<?>[] all = impl.getConstructors(); // Get all public constructors of the class.
+
+
+        // Filter out those that have the @Inject annotation.
+        Constructor<?>[] injectCtors = Arrays.stream(all)
+                .filter(c -> c.isAnnotationPresent(Inject.class))
+                .toArray(Constructor<?>[]::new);
+
+        // If there's exactly one @Inject constructor, use it.
+        if (injectCtors.length == 1) {
+            return injectCtors[0];
+        }
+        // If there are multiple @Inject constructors, that's an error.
+        if (injectCtors.length > 1) {
+            throw new ContainerException("Multiple @Inject constructors found for " + impl.getName());
+        }
+
+        // If there are no @Inject constructors, but exactly one public constructor, use that.
+        if (all.length == 1) {
+            return all[0];
+        }
+        // If there are no public constructors, that's an error.
+        if (all.length == 0) {
+            throw new ContainerException("No public constructors found for " + impl.getName());
+        }
+
+        // If there are multiple public constructors and none are annotated with @Inject, that's ambiguous.
+        throw new ContainerException("Ambiguous constructors for " + impl.getName()
+                + " (no @Inject and multiple public constructors)");
+    }
+}

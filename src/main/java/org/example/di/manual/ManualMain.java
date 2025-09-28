4ec32b26f28a1bd4a33f9549d7dae2e6ddc33fa3
Deansie <dennis-andersen@outlook.com>
2025-09-28 08:29:15 +0200

    feat(manual): create Main class for manual wiring

--- src/main/java/org/example/di/manual/ManualMain.java
+++ src/main/java/org/example/di/manual/ManualMain.java
@@ -1,4 +1,11 @@
 package org.example.di.manual;
 
+import org.example.di.common.GreetingsService;
+
 public class ManualMain {
+    public static void main(String[] args) {
+        InMemoryMessageRepository repo = new InMemoryMessageRepository();
+        GreetingsService service = new SimpleGreetingService(repo);
+        System.out.println(service.getGreeting("User"));
+    }
 }

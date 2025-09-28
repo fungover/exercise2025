48b8c5b0a9f69f770546e2fff04e17407734a9b4
Deansie <dennis-andersen@outlook.com>
2025-09-28 08:42:28 +0200

    test(container): add simple test for getInstance

--- src/test/java/org/example/di/container/SimpleContainerTest.java
+++ src/test/java/org/example/di/container/SimpleContainerTest.java
@@ -1,4 +1,21 @@
 package org.example.di.container;
+import org.example.di.common.GreetingsService;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
-public class SimpleContainerTest {
+class SimpleContainerTest {
+    @Test
+    void getInstance_shouldResolveClassWithoutDependencies() {
+        SimpleContainer container = new SimpleContainer();
+        MessageRepoImp repo = container.getInstance(MessageRepoImp.class);
+        assertThat(repo).isNotNull();
+        assertThat(repo.getMessage()).isEqualTo("Hi");
+    }
+    @Test
+    void getInstance_shouldRecursivelyResolveDependencies() {
+        SimpleContainer container = new SimpleContainer();
+        GreetingsService service = container.getInstance(GreetingServiceImp.class);
+        assertThat(service).isNotNull();
+        assertThat(service.getGreeting("Test")).isEqualTo("Hi, Test!");
+    }
 }

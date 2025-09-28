2d57d7984b8135809f00822c1263cc2ab880a05f
Magnus <49763392+MagnusTerak@users.noreply.github.com>
2025-09-28 16:00:28 +0200

    Product class is now an record instead. Added CategoryEnum for categories.

---
+++ src/main/java/org/example/entities/CategoryEnum.java
@@ -0,0 +1,7 @@
+package org.example.entities;
+
+public enum CategoryEnum {
+    UTILITY,
+    ELECTRIC,
+    BUILDING
+}
--- src/main/java/org/example/entities/Product.java
+++ src/main/java/org/example/entities/Product.java
@@ -2,20 +2,15 @@ package org.example.entities;
 
 import java.time.LocalDate;
 
-public class Product {
-    private int id;
-    private String name;
-    private Category category;
-    private int rating;
-    private LocalDate createdDate;
-    private LocalDate modifiedDate;
+public record Product(
+        int id,
+        String name,
+        CategoryEnum category,
+        int rating,
+        LocalDate createdDate,
+        LocalDate modifiedDate
+) {
+    public Product {
 
-    public Product(int id, String name, Category category, int rating, LocalDate createdDate, LocalDate modifiedDate) {
-        this.id = id;
-        this.name = name;
-        this.category = category;
-        this.rating = rating;
-        this.createdDate = createdDate;
-        this.modifiedDate = modifiedDate;
     }
 }

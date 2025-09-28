eeca6913316d45a8d16696924fabf2b94b37d858
Deansie <dennis-andersen@outlook.com>
2025-09-28 08:20:24 +0200

    test(manual): add simple tests for getMessage

--- src/test/java/org/example/di/manual/InMemoryMessageRepositoryTest.java
+++ src/test/java/org/example/di/manual/InMemoryMessageRepositoryTest.java
@@ -1,4 +1,13 @@
 package org.example.di.manual;
 
-public class InMemoryMessageRepositoryTest {
+import org.example.di.common.MessageRepository;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InMemoryMessageRepositoryTest {
+    @Test
+    void getMessage_shouldReturnFixedMessage() {
+        MessageRepository repo = new InMemoryMessageRepository();
+        assertThat(repo.getMessage()).isEqualTo("Hello");
+    }
 }
--- src/test/java/org/example/di/manual/SimpleGreetingServiceTest.java
+++ src/test/java/org/example/di/manual/SimpleGreetingServiceTest.java
@@ -1,4 +1,16 @@
 package org.example.di.manual;
 
-public class SimpleGreetingServiceTest {
+import org.example.di.common.GreetingsService;
+import org.example.di.common.MessageRepository;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleGreetingServiceTest {
+    @Test
+    void getGreeting_shouldCombineMessageAndName() {
+        MessageRepository repo = new InMemoryMessageRepository();
+        GreetingsService service = new SimpleGreetingService(repo);
+        assertThat(service.getGreeting("World")).isEqualTo("Hello, World!");
+
+    }
 }

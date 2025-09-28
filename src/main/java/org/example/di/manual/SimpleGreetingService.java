c3d38a7aa55bf2390f66c6db61cf81ab3a888366
Deansie <dennis-andersen@outlook.com>
2025-09-28 08:24:16 +0200

    feat(manual): add class SimpleGreetingService that implements GreetingService

--- src/main/java/org/example/di/manual/SimpleGreetingService.java
+++ src/main/java/org/example/di/manual/SimpleGreetingService.java
@@ -1,4 +1,16 @@
 package org.example.di.manual;
 
-public class SimpleGreetingService {
+import org.example.di.common.GreetingsService;
+import org.example.di.common.MessageRepository;
+
+public class SimpleGreetingService implements GreetingsService {
+    private final MessageRepository repository;
+    public SimpleGreetingService(MessageRepository repository) {
+        this.repository = repository;
+    }
+    @Override
+    public String getGreeting(String name) {
+        return repository.getMessage() + ", " + name + "!";
+    }
+
 }

50c8a73347695b82d35dd2d3327a35105249579f
Deansie <dennis-andersen@outlook.com>
2025-09-29 19:55:25 +0200

    feat(container): add GreetingServiceImp that implements GreetingsService

--- src/main/java/org/example/di/container/GreetingServiceImp.java
+++ src/main/java/org/example/di/container/GreetingServiceImp.java
@@ -1,4 +1,16 @@
 package org.example.di.container;
 
-public class GreetingServiceImp {
+import org.example.di.common.GreetingsService;
+import org.example.di.common.MessageRepository;
+
+public class GreetingServiceImp implements GreetingsService {
+    private final MessageRepository repository;
+    public GreetingServiceImp(MessageRepository repository) {
+        this.repository = repository;
+    }
+    @Override
+    public String getGreeting(String name) {
+        return repository.getMessage() + ", " + name + "!";
+    }
+
 }

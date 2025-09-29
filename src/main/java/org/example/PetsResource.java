0e34c01fae7ee3a9c394a888b6e381e19eded0aa
SpinalGlitter <johannes.persson47@gmail.com>
2025-09-29 13:00:23 +0200

    Get wildfy to work and started the project
    Co-authored-by: EmpyreanMist <christianfryksten1@gmail.com>
    Co-authored-by: marcusmoller97 <marcusssmoller@gmail.com>

---
+++ src/main/java/org/example/PetsResource.java
@@ -0,0 +1,42 @@
+package org.example;/*package org.example;*/
+
+/*@Path("/pets")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class PetsResource {
+
+    @Inject
+    Service service;
+
+    @POST
+    public Response adopt(@Valid PetDTO dto) {
+        PetDTO saved = service.adopt(dto);
+        return Response.status(Response.Status.CREATED).entity(saved).build();
+    }
+
+    @GET
+    public List<PetDTO> list() {
+        return service.list();
+    }
+}*/
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import org.example.Hello;
+
+@Path("hello")
+public class PetsResource {
+
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Hello hello(@QueryParam("name") String name) {
+        if ((name == null) || name.trim().isEmpty()) {
+            name = "world";
+        }
+
+        return new Hello(name);
+    }
+}

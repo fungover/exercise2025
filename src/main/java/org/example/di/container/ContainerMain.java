90529063eb76f812b115bf21c34bd55ed8e22697
Deansie <dennis-andersen@outlook.com>
2025-09-29 20:08:55 +0200

    feat(container): add ContainerMain for manual wiring

--- src/main/java/org/example/di/container/ContainerMain.java
+++ src/main/java/org/example/di/container/ContainerMain.java
@@ -1,4 +1,9 @@
 package org.example.di.container;
 
 public class ContainerMain {
+    public static void main(String[] args) {
+        SimpleContainer container = new SimpleContainer();
+        GreetingServiceImp service = container.getInstance(GreetingServiceImp.class);
+        System.out.println(service.getGreeting("User from container"));
+    }
 }

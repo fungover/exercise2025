d91e947304251741a09817caab08f2e941ca7334
Henrik <Henrikmattsson89@gmail.com>
2025-10-01 08:20:15 +0200

    mostly done with the different get/put/delete

--- src/main/java/org/example/resource/PetResource.java
+++ src/main/java/org/example/resource/PetResource.java
@@ -12,6 +12,7 @@ import org.example.dto.PetDTO;
 import org.example.service.PetService;
 
 import java.net.URI;
+import java.util.List;
 
 @Path("/pets")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -33,6 +34,42 @@ public class PetResource {
                        .entity(created)
                        .build();
 
+    }
+
+    @GET public Response listPets(@QueryParam("offset") Integer offset,
+                                  @QueryParam("limit") Integer limit,
+                                  @QueryParam("species") String species,
+                                  @QueryParam("sortBy") String sortBy,
+                                  @QueryParam("order") String order) {
+        List<PetDTO> list = petService.listPets(offset, limit, species, sortBy,
+          order);
+        return Response.ok(list)
+                       .build();
+    }
+
+    @GET @Path("{id}") public Response getPet(@PathParam("id") long id) {
+        return Response.ok(petService.getPet(id))
+                       .build();
+    }
+
+    @PUT @Path("{id}/feed") public Response feedPet(@PathParam("id") long id,
+                                                    @QueryParam("amount")
+                                                    @DefaultValue("10") int amount) {
+        return Response.ok(petService.feedPet(id, amount))
+                       .build();
+    }
+
+    @PUT @Path("{id}/play") public Response playPet(@PathParam("id") long id,
+                                                    @QueryParam("amount")
+                                                    @DefaultValue("10") int amount) {
+        return Response.ok(petService.playWithPet(id, amount))
+                       .build();
+    }
+
+    @DELETE @Path("{id}") public Response releasePet(@PathParam("id") long id) {
+        petService.releasePet(id);
+        return Response.noContent()
+                       .build();
     }
 
 }

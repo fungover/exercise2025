ed831edc0e2e6422f9a0ec7a5ac6022fa833ad99
Deansie <dennis-andersen@outlook.com>
2025-10-03 14:13:52 +0200

    test(cdi): extend testing to ensure same repository is used for every instance

--- src/test/java/org/example/di/cdi/CdiGreetingServiceTest.java
+++ src/test/java/org/example/di/cdi/CdiGreetingServiceTest.java
@@ -3,6 +3,7 @@ package org.example.di.cdi;
 import jakarta.enterprise.inject.se.SeContainer;
 import jakarta.enterprise.inject.se.SeContainerInitializer;
 import org.example.di.common.GreetingsService;
+import org.example.di.common.MessageRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,17 +14,33 @@ class CdiGreetingServiceTest {
 
     @BeforeEach
     void setUp() {
+        CdiMessageRepository.resetCounter();
         container = SeContainerInitializer.newInstance().initialize();
     }
 
     @AfterEach
     void tearDown() {
-        container.close();
+        if (container != null) {
+            container.close();
+        }
     }
 
     @Test
     void getGreeting_shouldUseInjectedRepository() {
         GreetingsService service = container.select(GreetingsService.class).get();
         assertThat(service.getGreeting("CDI-Tester")).isEqualTo("Hi there, CDI-Tester!");
+    }
+
+    @Test
+    void getGreeting_shouldUseSameRepositoryForEveryInstance() {
+        GreetingsService service1 = container.select(GreetingsService.class).get();
+        GreetingsService service2 = container.select(GreetingsService.class).get();
+        service1.getGreeting("CDI-Tester1");
+        service2.getGreeting("CDI-Tester2");
+        MessageRepository repo = container.select(MessageRepository.class).get();
+        MessageRepository repo2 = container.select(MessageRepository.class).get();
+        assertThat(service1).isNotSameAs(service2);
+        assertThat(repo).isSameAs(repo2);
+        assertThat(CdiMessageRepository.getCounter()).isEqualTo(1);
     }
 }

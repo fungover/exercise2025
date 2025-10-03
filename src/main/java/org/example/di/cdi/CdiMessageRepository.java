b209b1a7ad385bacebcead3f1319f96db0562d45
Deansie <dennis-andersen@outlook.com>
2025-10-03 14:15:47 +0200

    feat(CdiMessageRepository): implement instance counter

--- src/main/java/org/example/di/cdi/CdiMessageRepository.java
+++ src/main/java/org/example/di/cdi/CdiMessageRepository.java
@@ -5,6 +5,22 @@ import org.example.di.common.MessageRepository;
 
 @ApplicationScoped
 public class CdiMessageRepository implements MessageRepository {
+
+    private static int counter = 0;
+
+    public CdiMessageRepository() {
+        counter++;
+        System.out.println("CdiMessageRepository created, count: " + counter);
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public static void resetCounter() {
+        counter = 0;
+    }
+
     @Override
     public String getMessage() {
         return "Hi there";

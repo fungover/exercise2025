dc74316d5449dd9bafc397c2db38b365008cb588
Deansie <dennis-andersen@outlook.com>
2025-10-03 13:37:17 +0200

    feat(cdi): added CdiGreetingService that implements GreetingsService

---
+++ src/main/java/org/example/di/cdi/CdiGreetingService.java
@@ -0,0 +1,22 @@
+package org.example.di.cdi;
+
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
+import org.example.di.common.GreetingsService;
+import org.example.di.common.MessageRepository;
+
+@Dependent
+public class CdiGreetingService  implements GreetingsService {
+    private final MessageRepository repository;
+    @Inject
+    public CdiGreetingService(MessageRepository repository) {
+        if (repository == null) {
+            throw new IllegalArgumentException("Repository must not be null");
+            }
+        this.repository = repository;
+    }
+    @Override
+    public String getGreeting(String name) {
+        return repository.getMessage() + ", " + name + "!";
+    }
+}

68fd3377f86830d63f390c167299a14cc070cb5f
Deansie <dennis-andersen@outlook.com>
2025-10-03 08:37:46 +0200

    test(cdi): add simple test for CdiGreetingService

---
+++ src/test/java/org/example/di/cdi/CdiGreetingServiceTest.java
@@ -0,0 +1,25 @@
+package org.example.di.cdi;
+
+import jakarta.enterprise.inject.se.SeContainer;
+import jakarta.enterprise.inject.se.SeContainerInitializer;
+import org.example.di.common.GreetingsService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CdiGreetingServiceTest {
+    private SeContainer container;
+    @BeforeEach
+    void setUp() {
+        container = SeContainerInitializer.newInstance().initialize();
+    }
+    @AfterEach
+    void tearDown() {
+        container.close();
+    }
+    @Test
+    void getGreeting_shouldUseInjectedRepository() {
+        GreetingsService service = container.select(GreetingsService.class).get();
+        assertThat(service.getGreeting("CDI-Tester")).isEqualTo("Hi there, CDI-Tester!");
+    }

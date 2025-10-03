7f1804884e35f8ab905db1fc9c0444c6ad3f4b7f
Deansie <dennis-andersen@outlook.com>
2025-10-03 12:06:01 +0200

    test(cdi): refactor

--- src/test/java/org/example/di/cdi/CdiGreetingServiceTest.java
+++ src/test/java/org/example/di/cdi/CdiGreetingServiceTest.java
@@ -10,16 +10,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CdiGreetingServiceTest {
     private SeContainer container;
+
     @BeforeEach
     void setUp() {
         container = SeContainerInitializer.newInstance().initialize();
     }
+
     @AfterEach
     void tearDown() {
         container.close();
     }
+
     @Test
     void getGreeting_shouldUseInjectedRepository() {
         GreetingsService service = container.select(GreetingsService.class).get();
         assertThat(service.getGreeting("CDI-Tester")).isEqualTo("Hi there, CDI-Tester!");
     }
+}

7f1bd1c1acbfa7989c146a699bfb385a80bb54af
Rikard <rikardengstrom89@gmail.com>
2025-10-04 18:31:06 +0200

    Added test for blank species

--- src/test/java/org/example/dto/PetDTOTest.java
+++ src/test/java/org/example/dto/PetDTOTest.java
@@ -42,4 +42,11 @@ class PetDTOTest {
         Set<ConstraintViolation<PetDTO>> violations = validator.validate(pet); // Validate the pet
         assertEquals(1, violations.size()); // Expect 1 violation
     }
+
+    @Test
+    void invalidBlankSpecies() {
+        PetDTO pet = new PetDTO("Doris", "", 50, 50); // Invalid: blank species
+        Set<ConstraintViolation<PetDTO>> violations = validator.validate(pet); // Validate the pet
+        assertEquals(1, violations.size()); // Expect 1 violation
+    }
 }

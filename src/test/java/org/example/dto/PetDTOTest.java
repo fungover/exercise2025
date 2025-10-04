abbe43533e6681ff5c6569d3271b2759700f9016
Rikard <rikardengstrom89@gmail.com>
2025-10-04 18:26:03 +0200

    Wrote test for a valid pet. added dependencies in pom.xml

---
+++ src/test/java/org/example/dto/PetDTOTest.java
@@ -0,0 +1,31 @@
+package org.example.dto;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PetDTOTest {
+
+    private static Validator validator;
+
+    @BeforeAll
+    static void setupValidator() {
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            validator = factory.getValidator();
+        }
+    }
+
+    @Test
+    void validPet() {
+        PetDTO pet = new PetDTO("Doris", "Dog", 50, 70); // Valid pet
+        Set<ConstraintViolation<PetDTO>> violations = validator.validate(pet); // Validate the pet
+        assertEquals(0, violations.size()); // Expect no violations
+    }
+}

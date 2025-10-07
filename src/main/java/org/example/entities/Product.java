37bd38879a8e4df82d922686733b4e6a092f9453
Magnus <49763392+MagnusTerak@users.noreply.github.com>
2025-10-07 11:23:03 +0200

    Implemented setter methods for each field

--- src/main/java/org/example/entities/Product.java
+++ src/main/java/org/example/entities/Product.java
@@ -23,5 +23,35 @@ public record Product(
         private int rating;
         private LocalDate createdDate;
         private LocalDate modifiedDate;
+
+        public Builder id(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder category(CategoryEnum category) {
+            this.category = category;
+            return this;
+        }
+
+        public Builder rating(int rating) {
+            this.rating = rating;
+            return this;
+        }
+
+        public Builder createdDate(LocalDate createdDate) {
+            this.createdDate = createdDate;
+            return this;
+        }
+
+        public Builder modifiedDate(LocalDate modifiedDate) {
+            this.modifiedDate = modifiedDate;
+            return this;
+        }
     }
 }

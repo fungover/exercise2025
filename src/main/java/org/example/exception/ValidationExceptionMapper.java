0c51cff7ffa7740ef751c1512ea991ffb052fcc0
Patrik <pat_p3@hotmail.com>
2025-10-07 13:00:41 +0200

    Added custom exception mapper for validation errors

---
+++ src/main/java/org/example/exception/ValidationExceptionMapper.java
@@ -0,0 +1,29 @@
+package org.example.exception;
+
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Provider
+public class ValidationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+
+    @Override
+    public Response toResponse(ConstraintViolationException exception) {
+        Map<String, String> errors = new HashMap<>();
+
+        for (ConstraintViolation<?> violation : exception.getConstraintViolations()) {
+            errors.put("message", violation.getMessage());
+        }
+
+        return Response
+                .status(Response.Status.BAD_REQUEST)
+                .entity(errors)
+                .build();
+    }
+}

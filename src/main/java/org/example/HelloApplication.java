6a7cae2e856fee728d3c9ab9f5956f47d8f22c0f
RobTrb <Robert.trb@gmail.com>
2025-10-08 12:48:33 +0200

    first commit. Initialised settings

--- src/main/java/org/example/HelloApplication.java
+++ src/main/java/org/example/HelloApplication.java
@@ -3,7 +3,7 @@ package org.example;
 import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.ApplicationPath;
 
-@ApplicationPath("api")
+@ApplicationPath("rest")
 public class HelloApplication extends Application {
     // Needed to enable Jakarta REST and specify path.
 }

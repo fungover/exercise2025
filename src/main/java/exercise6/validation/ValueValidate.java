344fc15c9ad1fd2f952a51ef41de0217aae90224
Louise <emmalouisekarlsson@hotmail.com>
2025-10-08 13:30:04 +0200

    Adjusted data type from Integer to int.

--- src/main/java/exercise6/validation/ValueValidate.java
+++ src/main/java/exercise6/validation/ValueValidate.java
@@ -8,12 +8,12 @@ public class ValueValidate {
     @NotNull(message = "Value must be entered")
     @Min(value = 1, message = "Value can not be less than 1")
     @Max(value = 100, message = "Value can not exceed 100")
-    private Integer requestValue;
+    private int requestValue;
 
     public int getRequestValue() {
         return requestValue;
     }
-    public void setRequestValue(Integer requestValue) {
+    public void setRequestValue(int requestValue) {
         this.requestValue = requestValue;
     }
 

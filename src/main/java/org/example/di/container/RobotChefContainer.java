cb10ed0b71b6444f6c678e4020d3f2690ec4a3d9
Calle <ca.karlsson@hotmail.com>
2025-10-09 17:13:09 +0200

    Add RobotChefContainer for dependency injection and type mapping

---
+++ src/main/java/org/example/di/container/RobotChefContainer.java
@@ -0,0 +1,47 @@
+package org.example.di.container;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RobotChefContainer {
+    public final Map<Class<?>, Class<?>> typeMappings = new HashMap<>();
+
+    public <T> void register(Class<T> baseType, Class<? extends T> implType) {
+        typeMappings.put(baseType, implType);
+    }
+
+
+    public <T> T getInstance(Class<T> type) {
+        try {
+            System.out.println("[Skapar robot instans av:] " + type.getSimpleName());
+
+            if (type.isInterface() || Modifier.isAbstract(type.getModifiers())) {
+                Class<?> impl = typeMappings.get(type);
+                if (impl == null) {
+                    throw new RuntimeException("Ingen binding hittades för " + type);
+                }
+                type = (Class<T>) impl;
+            }
+
+            Constructor<?> constructor = type.getDeclaredConstructors()[0];
+            constructor.setAccessible(true);
+            Class<?>[] parameterTypes = constructor.getParameterTypes();
+
+            if (parameterTypes.length == 0) {
+                return (T) constructor.newInstance();
+            }
+
+            Object[] dependencies = Arrays.stream(parameterTypes)
+                    .map(this::getInstance)
+                    .toArray();
+
+            return (T) constructor.newInstance(dependencies);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

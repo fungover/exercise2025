dfad5c1ab424c25c1118b67928c353757b9f9ea0
Calle <ca.karlsson@hotmail.com>
2025-10-09 17:15:37 +0200

    Add dependency injection update to Resturant class for RobotChefContainer

--- src/main/java/org/example/di/Resturant.java
+++ src/main/java/org/example/di/Resturant.java
@@ -1,15 +1,26 @@
 package org.example.di;
 
-public class Resturant {
-    static void main(String[] args) {
+import org.example.di.container.RobotChefContainer;
 
+public class Resturant {
+    public static void main(String[] args) {
+
+        // Without DI container
         IngredientSupplier supplier = new LocalIngredientSupplier();
         BurgerMaker burgerChef = new BurgerChef(supplier);
         PizzaMaker pizzaChef = new PizzaChef(supplier);
-
         HeadChef head = new HeadChef(burgerChef, pizzaChef);
-
         head.serveBurger("Kevin");
         head.servePizza("Bob");
+
+        // With DI container
+        RobotChefContainer robotChef = new RobotChefContainer();
+        robotChef.register(IngredientSupplier.class, LocalIngredientSupplier.class);
+        robotChef.register(BurgerMaker.class, BurgerChef.class);
+        robotChef.register(PizzaMaker.class, PizzaChef.class);
+        HeadChef roboChef = robotChef.getInstance(HeadChef.class);
+
+        roboChef.serveBurger("Kevin");
+        roboChef.servePizza("Bob");
     }
 }

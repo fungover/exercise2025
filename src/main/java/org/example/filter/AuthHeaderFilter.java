4b7e96ae542103868f5f30572ae68304f8ce8e3e
Martin Blomberg <martin.blomberg@outlook.com>
2025-10-10 11:41:06 +0200

    Add filter for auth

---
+++ src/main/java/org/example/filter/AuthHeaderFilter.java
@@ -0,0 +1,21 @@
+package org.example.filter;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+@Provider
+public class AuthHeaderFilter implements ContainerRequestFilter {
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext) throws IOException {
+        String authHeader =containerRequestContext.getHeaderString("X-Auth");
+        if( authHeader == null || !authHeader.equals("secret"))
+            containerRequestContext.abortWith(
+                    Response.status(Response.Status.UNAUTHORIZED)
+                            .entity("Missing or invalid auth token")
+                            .build());
+    }
+}

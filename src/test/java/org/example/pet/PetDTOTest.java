5e618d047d73936dd73ad06f046ddb8de190a66a
ViktorNoskire <vikkerinho@gmail.com>
2025-10-12 19:17:05 +0200

    Create simple test for violations

---
+++ src/test/java/org/example/pet/PetDTOTest.java
@@ -0,0 +1,34 @@
+package org.example.pet;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetDTOTest {
+	@Test
+	void testAdoptingANewPet() {
+		Pet pet = new PetDTO("Fido", "dog");
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+		Set<ConstraintViolation<Pet>> violations = validator.validate(pet);
+
+		assertTrue(violations.isEmpty());
+	}
+
+	@Test
+	void testAdoptingAnInvalidPet() {
+		Pet pet = new PetDTO("Fido", "Cow");
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+		Set<ConstraintViolation<Pet>> violations = validator.validate(pet);
+		assertThat(violations.size()).isEqualTo(1);
+		assertThat(violations.iterator().next().getMessage()).isEqualTo("Invalid species");
+	}
+}

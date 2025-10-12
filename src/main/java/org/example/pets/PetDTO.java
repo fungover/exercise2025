ebd41a093243c7a8e4437ed898c521d16f0a316f
nilsishome <nilsnyberg24@gmail.com>
2025-10-12 11:11:20 +0200

    Set default values for hunger and happiness to pets

--- src/main/java/org/example/pets/PetDTO.java
+++ src/main/java/org/example/pets/PetDTO.java
@@ -1,4 +1,9 @@
 package org.example.pets;
 
 public record PetDTO(String name, String species,
-                     int hungerLevel, int happiness) {}
+                     int hungerLevel, int happiness) {
+    public PetDTO {
+        hungerLevel = 5;
+        happiness = 5;
+    }
+}

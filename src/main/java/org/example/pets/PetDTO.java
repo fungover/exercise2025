2e6aa151528779c47169416d6c2825cc99ce7e81
nilsishome <nilsnyberg24@gmail.com>
2025-10-13 16:10:20 +0200

    Remove hungerLevel and happiness presets

--- src/main/java/org/example/pets/PetDTO.java
+++ src/main/java/org/example/pets/PetDTO.java
@@ -1,9 +1,4 @@
 package org.example.pets;
 
 public record PetDTO(String name, String species,
-                     int hungerLevel, int happiness) {
-    public PetDTO {
-        hungerLevel = 5;
-        happiness = 5;
-    }
-}
+                     int hungerLevel, int happiness) {}

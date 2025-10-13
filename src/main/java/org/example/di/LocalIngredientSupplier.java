89fd5ccdf0ce155a7d9863d63d525e90cbc77495
Calle <ca.karlsson@hotmail.com>
2025-10-13 11:05:25 +0200

    Add @ApplicationScoped annotation to LocalIngredientSupplier for dependency injection

--- src/main/java/org/example/di/LocalIngredientSupplier.java
+++ src/main/java/org/example/di/LocalIngredientSupplier.java
@@ -1,5 +1,8 @@
 package org.example.di;
 
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class LocalIngredientSupplier implements IngredientSupplier {
 
     @Override

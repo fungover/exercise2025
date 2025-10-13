0d4efb9fd0cf26c1e39ed9b83e81cf9c67342df6
0x0xoscar <139160108+0x0xoscar@users.noreply.github.com.>
2025-10-13 14:54:56 +0200

    Added Application Root

---
+++ src/main/java/org/fungover/config/JAXRSConfiguration.java
@@ -0,0 +1,8 @@
+package org.fungover.config;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("api/v1")
+public class JAXRSConfiguration extends Application {
+}

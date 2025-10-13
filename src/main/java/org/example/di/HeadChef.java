62b749f06d34b5f599f0016b0c971bbe26be767b
Calle <ca.karlsson@hotmail.com>
2025-10-13 11:05:34 +0200

    Add @Dependent and @Inject annotations to HeadChef for dependency injection

--- src/main/java/org/example/di/HeadChef.java
+++ src/main/java/org/example/di/HeadChef.java
@@ -1,9 +1,14 @@
 package org.example.di;
 
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
+
+@Dependent
 public class HeadChef {
     private final BurgerMaker burgerMaker;
     private final PizzaMaker pizzaMaker;
 
+    @Inject
     public HeadChef(BurgerMaker burgerMaker, PizzaMaker pizzaMaker) {
         this.burgerMaker = burgerMaker;
         this.pizzaMaker = pizzaMaker;

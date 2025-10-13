aea8c29d6568fb0c2a7e8a81c3dad84c86f6b1b2
Calle <ca.karlsson@hotmail.com>
2025-10-13 11:12:17 +0200

    Add clarifying comment in App to redirect to Restaurants.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class App {
+    // Not used, Restaurants are used instead.
     public static void main(String[] args) {
         System.out.println("Hello There!");
     }

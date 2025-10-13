9994dab24ba8a9520eaea37f49fd32642cfd07da
nilsishome <nilsnyberg24@gmail.com>
2025-10-13 18:18:26 +0200

    Fix typo in PlayDecorator record

--- src/main/java/org/example/pets/decorator/PlayDecorator.java
+++ src/main/java/org/example/pets/decorator/PlayDecorator.java
@@ -5,7 +5,7 @@ import org.example.pets.PetDTO;
 public record PlayDecorator(PetDTO petDTO) {
 
     public PlayDecorator(PetDTO petDTO) {
-        this.petDTO = new PetDTO(petDTO().name(),  petDTO.species(),
+        this.petDTO = new PetDTO(petDTO.name(),  petDTO.species(),
                 petDTO.hungerLevel(), petDTO.happiness() + 1);
     }
 

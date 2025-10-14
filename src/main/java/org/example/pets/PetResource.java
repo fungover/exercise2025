911c1547c59bd507fe0fb3a9ba88840331312098
Taru Keskinen <taru.keskinen@lerniastudent.se>
2025-10-14 13:00:22 +0200

    Update methods for adopting and listing pets and tested in postman

--- src/main/java/org/example/pets/PetResource.java
+++ src/main/java/org/example/pets/PetResource.java
@@ -1,7 +1,10 @@
 package org.example.pets;
 
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.example.Hello;
 
 import java.util.ArrayList;
@@ -22,19 +25,19 @@ DELETE /pets/{id} → Release the pet
 @Consumes(MediaType.APPLICATION_JSON)
 public class PetResource {
 
+    @Inject
+    private PetService petService;
 
-    private static List<PetDTO> pets = new ArrayList<>();
-
-    // GET /api/pets → lista alla djur
+    // GET /api/pets show all pets
     @GET
-    public List<PetDTO> listAll() {
-        return pets;
+    public List <PetDTO> listAll(){
+        return petService.getAllPets();
     }
 
-    // POST /api/pets → lägg till nytt djur
+    // POST /api/pets  add new pet
     @POST
-    public PetDTO addPet(PetDTO pet) {
-        pets.add(pet);
-        return pet;
+    public Response addPet(@Valid PetDTO pet) {
+        PetDTO savedPet = petService.addPet(pet);
+        return Response.status(Response.Status.CREATED).entity(savedPet).build();
     }
 }
--- src/main/java/org/example/pets/PetService.java
+++ src/main/java/org/example/pets/PetService.java
@@ -1,10 +1,31 @@
 package org.example.pets;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
 /*
 Create a thread-safe service class to manage Pets:
 Use ConcurrentHashMap<Long, PetDTO> or CopyOnWriteArrayList
 Optionally use ReentrantLock for atomic updates (e.g., feeding or playing)
 */
 
+@ApplicationScoped
 public class PetService {
+    private final Map<Long, PetDTO> pets = new ConcurrentHashMap<>();
+    private final AtomicLong nextId = new AtomicLong(1);
+
+    // Adopt pet
+    public PetDTO addPet(PetDTO pet) {
+        long id = nextId.getAndIncrement();
+        pet.setId(id);
+        pets.put(id, pet);
+        return pet;
+    }
+
+    // List all pets
+    public List<PetDTO> getAllPets() {
+        return new ArrayList<>(pets.values());
+    }
 }

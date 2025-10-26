a54f578b3ce22fdfa1a1a18cb17c1ad01b55876f
Magnus <49763392+MagnusTerak@users.noreply.github.com>
2025-10-26 15:32:35 +0100

    Added validation for DTO

--- src/main/java/org/example/PetDTO.java
+++ src/main/java/org/example/PetDTO.java
@@ -1,9 +1,19 @@
 package org.example;
+import jakarta.validation.constraints.*;
 
 public class PetDTO {
+    @NotBlank(message = "Name can't be blank")
     private String name;
+
+    @NotBlank(message = "Species can't be blank")
     private String species;
+
+    @Min(value = 0, message = "Hunger can't go below 0")
+    @Max(value = 100, message = "Hunger can't go above 100")
     private int hungerLevel;
+
+    @Min(value = 0, message = "Happiness can't be below 0")
+    @Max(value = 100, message = "Happiness can't go above 100")
     private int happiness;
 
     public PetDTO(String name, String species, int hungerLevel, int happiness) {

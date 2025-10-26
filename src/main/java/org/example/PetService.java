fe196a4821c2e854b6e409b4d8dc85ef54034cf2
Magnus <49763392+MagnusTerak@users.noreply.github.com>
2025-10-26 15:51:23 +0100

    Added PetService class, to manage pets, with hashMap & ReentrantLock

---
+++ src/main/java/org/example/PetService.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class PetService {
+    private final ConcurrentHashMap<Long, PetDTO> pets = new ConcurrentHashMap<>();
+    private final ReentrantLock lock = new ReentrantLock();
+
+    public void addPet(Long id, PetDTO pet) {
+        pets.put(id, pet);
+    }
+
+    public PetDTO getPet(Long id) {
+        return pets.get(id);
+    }
+
+    public ConcurrentHashMap<Long, PetDTO> getPets() {
+        return new ConcurrentHashMap<>(pets);
+    }
+
+    public void removePet(Long id) {
+        pets.remove(id);
+    }
+
+    public boolean feedPet(PetDTO pet) {
+        lock.lock();
+
+        try {
+            PetDTO pet = pets.get(id);
+
+            if (pet == null) {
+                pet.setHungerLevel(pet.getHungerLevel() + 10);
+                System.out.println("You fed " + pet.getName() + ". " +  pet.getName() + " hunger is now at " + pet.getHungerLevel() + "%");
+                return true;
+            }
+            return false;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public boolean playPet(Long id) {
+        lock.lock();
+        try {
+            PetDTO pet = pets.get(id);
+            if (pet == null) {
+                pet.setHappiness(pet.getHappiness() + 10);
+                System.out.println("You played with " +  pet.getName() + ". " + pet.getName() + " is now " + pet.getHappiness() + "% happy");
+                return true;
+            }
+            return false;
+        } finally {
+            lock.unlock();
+        }
+    }
+}

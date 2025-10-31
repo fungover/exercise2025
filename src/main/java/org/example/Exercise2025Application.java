67bc482862fe8b97c1e6260407309a97d8f6f551
Alfred Brännare <alfred-brannare@hotmail.com>
2025-10-31 15:00:11 +0100

    chore: setup MySQL - Add docker-compose.yml - Configure application.properties - Add MySQL connector dependency

--- src/main/java/org/example/Exercise2025Application.java
+++ src/main/java/org/example/Exercise2025Application.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Exercise2025Application {
-
-    void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(Exercise2025Application.class, args);
     }
 

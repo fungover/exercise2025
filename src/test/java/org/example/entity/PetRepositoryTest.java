40b73934710c7ade4aae56f692ade42dbcb56ec3
Deansie <dennis-andersen@outlook.com>
2025-11-02 13:36:42 +0100

    test(PetRepositoryTest.java): add test for findAll()

--- src/test/java/org/example/entity/PetRepositoryTest.java
+++ src/test/java/org/example/entity/PetRepositoryTest.java
@@ -1,15 +1,19 @@
 package org.example.entity;
 
+import org.example.repository.PetRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class PetRepositoryTest {
 
-    @Autowired PetRepository repo;
+    @Autowired
+    PetRepository repo;
 
     @Test
     void persistAndFind(){
@@ -17,5 +21,11 @@ class PetRepositoryTest {
         Pet saved = repo.save(p);
         assertThat(saved.getId()).isNotNull();
         assertThat(repo.findById(saved.getId()).get().getName()).isEqualTo("Test");
+    }
+
+    @Test
+    void findAll() {
+        repo.save(Pet.builder().name("AA").species("B").build());
+        assertThat(repo.findAll()).hasSize(1);
     }
 }

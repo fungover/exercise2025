9f3ca6381b2f2d1db8e17e6516cd352e024a3e64
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2025-11-03 22:29:12 +0100

    Chore: Add Spring Boot Project (exercise8)

---
+++ exercise8/src/main/java/com/example/ex8/Exercise8Application.java
@@ -0,0 +1,13 @@
+package com.example.ex8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise8Application.class, args);
+	}
+
+}

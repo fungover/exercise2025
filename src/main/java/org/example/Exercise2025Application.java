31ca149e6ab3cd853b6757cf009d98912b1b284f
Rikard <rikardengstrom89@gmail.com>
2025-11-03 14:24:25 +0100

    pushing branch

--- src/main/java/org/example/Exercise2025Application.java
+++ src/main/java/org/example/Exercise2025Application.java
@@ -8,6 +8,7 @@ public class Exercise2025Application {
 
     void main(String[] args) {
         SpringApplication.run(Exercise2025Application.class, args);
+        //Bla bla bla testing testing
     }
 
 }

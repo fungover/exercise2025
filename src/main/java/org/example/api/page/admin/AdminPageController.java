05f42d7ad5a2fb818596c2f2e1e1e271f5374356
Alfred Brännare <alfred-brannare@hotmail.com>
2025-11-04 21:40:51 +0100

    feat: implement update director, update movie, add movie and add director http templates

--- src/main/java/org/example/api/page/admin/AdminPageController.java
+++ src/main/java/org/example/api/page/admin/AdminPageController.java
@@ -1,6 +1,8 @@
 package org.example.api.page.admin;
 
+import org.example.entities.Movie;
 import org.example.service.director.DirectorService;
+import org.example.service.movie.MovieService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,15 +15,20 @@ import java.util.List;
 @RequestMapping("/admin")
 public class AdminPageController {
     private final DirectorService directorService;
+    private final MovieService movieService;
 
-    public AdminPageController(DirectorService directorService) {
+    public AdminPageController(DirectorService directorService, MovieService movieService) {
         this.directorService = directorService;
+        this.movieService = movieService;
     }
 
     @GetMapping
     public String adminDashboard(Model model) {
         List<Director> directors = directorService.getAllDirectors();
+        List<Movie> movies = movieService.getAllMovies();
+
         model.addAttribute("directors", directors);
+        model.addAttribute("movies", movies);
 
         return "admin/admin";
     }

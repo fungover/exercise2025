ecbb8e302590457f1ccdb2b399a3a30d84f8e043
Rikard <rikardengstrom89@gmail.com>
2025-11-06 12:10:47 +0100

    Added domain package and exerciseEntity class

---
+++ src/main/java/org/example/domain/ExerciseEntity.java
@@ -0,0 +1,37 @@
+package org.example.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "exercises")
+public class ExerciseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    @Column(name = "muscle_group", length = 100)
+    private String muscleGroup;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMuscleGroup() {
+        return muscleGroup;
+    }
+
+    public void setMuscleGroup(String muscleGroup) {
+        this.muscleGroup = muscleGroup;
+    }
+}

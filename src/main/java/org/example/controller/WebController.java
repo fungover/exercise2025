9cc884d7bdd886df8100d12039a02afb4b3a8d1f
Deansie <dennis-andersen@outlook.com>
2025-11-09 11:56:04 +0100

    feat(controller): add WebController with Thymeleaf support to render index.html with dynamic pageTitle

---
+++ src/main/java/org/example/controller/WebController.java
@@ -0,0 +1,15 @@
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("pageTitle", "\uD83D\uDC3E Pet Adoption Service \uD83D\uDC3E");
+
+        return "index";
+    }
+}

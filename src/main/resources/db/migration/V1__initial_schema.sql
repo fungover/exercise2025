01bfa2419155c91bd79eb88178317f287185c0ac
Alfred Brännare <alfred-brannare@hotmail.com>
2025-11-10 16:13:06 +0100

    chore: implement Flyway

---
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -0,0 +1,28 @@
+-- Create director table
+CREATE TABLE director
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name  VARCHAR(255) NOT NULL
+);
+
+-- Create movie table
+CREATE TABLE movie
+(
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title       VARCHAR(255) NOT NULL,
+    duration    BIGINT       NOT NULL,
+    genre       VARCHAR(255) NOT NULL,
+    director_id BIGINT       NOT NULL,
+    CONSTRAINT fk_movie_director FOREIGN KEY (director_id) REFERENCES director (id)
+);
+
+-- Create user table
+CREATE TABLE user
+(
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username    VARCHAR(255),
+    password    VARCHAR(255),
+    authorities VARCHAR(255),
+    api_key     VARCHAR(255)
+);

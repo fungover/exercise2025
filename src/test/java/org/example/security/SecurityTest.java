89dd038faf79169b28b8deadcf4e5af8e6e740e8
Tobias Larsson <tobbe-larsson@hotmail.com>
2025-11-11 10:01:45 +0100

    Renamed `Exercise2025ApplicationTest` to `TobiasApplicationTest` and added `SecurityTest` to verify unauthenticated POST request rejection.

---
+++ src/test/java/org/example/security/SecurityTest.java
@@ -0,0 +1,33 @@
+package org.example.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SecurityTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldRejectUnauthenticatedPost() throws Exception {
+        String json = """
+                {
+                    "name": "UnauthorizedProduct",
+                    "sku": "UN-001",
+                    "quantity": 5
+                }
+                """;
+        mockMvc.perform(post("/products")
+                .contentType("application/json")
+                .content(json))
+                .andExpect(status().isUnauthorized());
+    }
+}

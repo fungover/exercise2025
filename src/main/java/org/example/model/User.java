c8d17469aeb89a8f9ae1b03954d6c403f7eb441a
Taru Keskinen <taru.keskinen@lerniastudent.se>
2025-11-17 13:33:04 +0100

    add constructors to User entity

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -15,4 +15,13 @@ public class User {
     private String username;
     private String email;
     private String password;
+
+    public User() {
+    }
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
 }

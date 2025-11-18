f291c2f350426c1bad40fbda7b3c812c66d5cbd7
Taru Keskinen <taru.keskinen@lerniastudent.se>
2025-11-18 10:35:09 +0100

    comments to classes for my own learning process

--- src/main/java/org/example/service/WorkoutService.java
+++ src/main/java/org/example/service/WorkoutService.java
@@ -4,6 +4,7 @@ import org.example.model.Workout;
 
 import java.util.List;
 
+// detta ska service KUNNA göra
 public interface WorkoutService {
     List<Workout> getAllWorkouts();
     Workout createWorkout(Workout workout);
--- src/main/java/org/example/service/WorkoutServiceImpl.java
+++ src/main/java/org/example/service/WorkoutServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+// Här skriver jag hur metoderna faktiskt FUNGERAR
 @Service
 public class WorkoutServiceImpl implements WorkoutService {
 

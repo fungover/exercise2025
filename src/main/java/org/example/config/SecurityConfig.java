5e1d2e1eadbb4ef24089faaf964c04aea9b06cb6
Deansie <dennis-andersen@outlook.com>
2025-11-19 22:03:44 +0100

    fix(config): disable csrf protection since its not working well with BasicAuth

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package org.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,13 +20,12 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.GET, "/pets", "/pets/*").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/pets").authenticated()
-                        .requestMatchers(HttpMethod.PUT, "/pets/**").authenticated()
-                        .requestMatchers(HttpMethod.DELETE, "/pets/**").authenticated()
+                        .requestMatchers("/pets").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());
+                .csrf(csrf -> csrf.disable())
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults());
         return http.build();
     }
 

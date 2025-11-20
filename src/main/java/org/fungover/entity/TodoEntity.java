70b1e7561a3c0ae2e9033484183a6d3f317c1297
0x0xoscar <139160108+0x0xoscar@users.noreply.github.com.>
2025-11-20 13:23:16 +0100

    Added Entity

---
+++ src/main/java/org/fungover/entity/TodoEntity.java
@@ -0,0 +1,71 @@
+package org.fungover.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="Todo")
+public class TodoEntity {
+
+    protected TodoEntity() {
+
+    }
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="completed")
+    private boolean completed;
+
+    @Column(name="due_date")
+    private LocalDate dueDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+}

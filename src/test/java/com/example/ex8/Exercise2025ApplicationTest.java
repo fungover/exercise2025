eeb245c699a141cbf0dbe4622945ea75b605f6b8
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2025-11-20 17:00:06 +0100

    Fix: H2 testconfig and run contextLoads with testprofile

--- src/test/java/com/example/ex8/Exercise2025ApplicationTest.java
+++ src/test/java/com/example/ex8/Exercise2025ApplicationTest.java
@@ -2,8 +2,10 @@ package com.example.ex8;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Exercise2025ApplicationTest {
 
     @Test

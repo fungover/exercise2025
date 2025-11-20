bde8d711d3550756f93ed49cde7256d2c9e79007
0x0xoscar <139160108+0x0xoscar@users.noreply.github.com.>
2025-11-20 13:52:24 +0100

    Added super simple custom exception

--- src/main/java/org/fungover/service/TodoService.java
+++ src/main/java/org/fungover/service/TodoService.java
@@ -1,4 +1,56 @@
 package org.fungover.service;
 
+import org.fungover.dto.Todo;
+import org.fungover.entity.TodoEntity;
+import org.fungover.mapper.TodoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    @Autowired
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public Todo create(Todo todoDTO) {
+        TodoEntity entity = TodoMapper.todoEntity(todoDTO);
+        entity.setId(null);
+        TodoEntity saved = todoRepository.save(entity);
+        return TodoMapper.todoDTO(saved);
+    }
+
+    public List<Todo> findAll() {
+        return todoRepository.findAll().stream().map(TodoMapper::todoDTO).collect(Collectors.toList());
+    }
+
+    public Todo findById(Long id) {
+        TodoEntity entity = todoRepository.findById(id).orElse(null); // will change
+        return TodoMapper.todoDTO(entity);
+    }
+
+    public Todo update(Long id, Todo todoDTO) {
+        TodoEntity existing = todoRepository.findById(id).orElse(null); // will change
+
+        existing.setTitle(todoDTO.title());
+        existing.setDescription(todoDTO.description());
+        existing.setCompleted(todoDTO.completed());
+        existing.setDueDate(todoDTO.dueDate());
+
+        TodoEntity saved = todoRepository.save(existing);
+        return TodoMapper.todoDTO(saved);
+    }
+
+    public void delete(Long id) {
+        if (!todoRepository.existsById(id)) {
+            // will add custom exception
+        }
+        todoRepository.deleteById(id);
+    }
 }

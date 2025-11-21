0fcdf0531eb74395c735d00d092d09e13b8cc930
Patrik <pat_p3@hotmail.com>
2025-11-21 16:13:53 +0100

    Fix the main method signature

--- src/main/java/org/example/PetApplication.java
+++ src/main/java/org/example/PetApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PetApplication {
 
-    void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(PetApplication.class, args);
     }
 
